6f31d128883d3499a3011964efee97523ae4b177
Robert Speck <r.speck@fz-juelich.de>
2014-12-29 17:37:24 +0100

    commenting the new code... more fixmes

--- examples/heat1d/parallel_playground.py
+++ examples/heat1d/parallel_playground.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     pparams['nu'] = 0.1
     pparams['nvars'] = [255,127]
 
+    # Fill description dictionary for easy hierarchy creation
     description = {}
     description['problem_class'] = heat1d
     description['problem_params'] = pparams
@@ -37,19 +38,27 @@ if __name__ == "__main__":
     description['level_params'] = lparams
     description['transfer_class'] = mesh_to_mesh_1d
 
+    # quickly generate block of steps
     MS = mp.generate_steps(num_procs,sparams,description)
 
+    # setup parameters "in time"
     t0 = 0
     Tend = 0.5
     dt = 0.125
 
+    # get initial values on finest level
     P = MS[0].levels[0].prob
     uinit = P.u_exact(t0)
 
+    # call main function to get things done...
     uend,step_stats = mp.run_pfasst_serial(MS,u0=uinit,t0=t0,dt=dt,Tend=Tend)
 
+    # compute exact solution and compare
     uex = P.u_exact(Tend)
+
     print('error at time %s: %s' %(Tend,np.linalg.norm(uex.values-uend.values,np.inf)/np.linalg.norm(
         uex.values,np.inf)))
+
+    # testing stats output...
     print(len(step_stats),len(step_stats[1].level_stats),len(step_stats[-1].level_stats[0].iter_stats))
     print(step_stats[1].residual,step_stats[0].level_stats[0].iter_stats[0].residual)
