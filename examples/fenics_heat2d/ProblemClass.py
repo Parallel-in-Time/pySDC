bee322f800613313f25e259dc54d90c37ca1118e
Robert Speck <robert@zam2092.zam.kfa-juelich.de>
2015-03-19 11:45:09 +0100

    definition of ProblemClass and preps for the rest

---
+++ examples/fenics_heat2d/ProblemClass.py
@@ -0,0 +1,155 @@
+from __future__ import division
+import numpy as np
+import dolfin as df
+
+from pySDC.Problem import ptype
+from pySDC.datatype_classes.fenics_mesh import fenics_mesh, rhs_fenics_mesh
+
+class fenics_heat2d(ptype):
+    """
+    Example implementing the forced 1D heat equation with Dirichlet-0 BC in [0,1]
+
+    Attributes:
+        A: second-order FD discretization of the 1D laplace operator
+        dx: distance between two spatial nodes
+    """
+
+    def __init__(self, cparams, dtype_u, dtype_f):
+        """
+        Initialization routine
+
+        Args:
+            cparams: custom parameters for the example
+            dtype_u: particle data type (will be passed parent class)
+            dtype_f: acceleration data type (will be passed parent class)
+        """
+
+        class Boundary(df.SubDomain):  # define the Dirichlet boundary
+            def inside(self, x, on_boundary):
+                    return on_boundary
+
+        # these parameters will be used later, so assert their existence
+        assert 'nvars' in cparams
+        assert 'alpha' in cparams
+        assert 'beta' in cparams
+        assert 't0' in cparams
+
+        # add parameters as attributes for further reference
+        for k,v in cparams.items():
+            setattr(self,k,v)
+
+        # invoke super init, passing number of dofs, dtype_u and dtype_f
+        super(fenics_heat2d,self).__init__(self.nvars,dtype_u,dtype_f)
+
+        mesh = df.UnitSquareMesh(self.nvars[0],self.nvars[1])
+        self.V = df.FunctionSpace(mesh, 'Lagrange', 1)
+        # Laplace terms
+        u = df.TrialFunction(self.sV)
+        v = df.TestFunction(self.V)
+        a_K = df.inner(df.nabla_grad(u), df.nabla_grad(v))*df.dx
+
+        # "Mass matrix" term
+        a_M = u*v*df.dx
+
+        self.M = df.assemble(a_M)
+        self.K = df.assemble(a_K)
+
+        self.u0 = df.Expression('1 + x[0]*x[0] + alpha*x[1]*x[1] + beta*t',alpha=self.alpha, beta=self.beta, t=self.t0)
+        self.g = df.Expression('beta - 2 - 2*alpha', beta=self.beta, alpha=self.alpha)
+
+        boundary = Boundary()
+        self.bc = df.DirichletBC(self.V, self.u0, boundary)
+
+
+
+    def solve_system(self,rhs,factor,u0,t):
+        """
+        Simple linear solver for (I-dtA)u = rhs
+
+        Args:
+            rhs: right-hand side for the nonlinear system
+            factor: abbrev. for the node-to-node stepsize (or any other factor required)
+            u0: initial guess for the iterative solver (not used here so far)
+
+        Returns:
+            solution as mesh
+        """
+
+        A = self.M + factor*self.K
+
+        self.u0.t = t
+        self.bc.apply(A,rhs)
+
+        u = fenics_mesh(u0)
+        df.solve(A,u.vector(),rhs)
+
+        return u
+
+
+    def __eval_fexpl(self,u,t):
+        """
+        Helper routine to evaluate the explicit part of the RHS
+
+        Args:
+            u: current values (not used here)
+            t: current time
+
+        Returns:
+            explicit part of RHS
+        """
+
+        # self.g.t = t
+        fexpl = df.interpolate(self.g,self.V)
+
+        return fexpl
+
+    def __eval_fimpl(self,u,t):
+        """
+        Helper routine to evaluate the implicit part of the RHS
+
+        Args:
+            u: current values
+            t: current time (not used here)
+
+        Returns:
+            implicit part of RHS
+        """
+
+        fimpl = self.K*u.vector()
+
+        return fimpl
+
+
+    def eval_f(self,u,t):
+        """
+        Routine to evaluate both parts of the RHS
+
+        Args:
+            u: current values
+            t: current time
+
+        Returns:
+            the RHS divided into two parts
+        """
+
+        f = rhs_fenics_mesh(self.nvars)
+        f.impl = self.__eval_fimpl(u,t)
+        f.expl = self.__eval_fexpl(u,t)
+        return f
+
+
+    def u_exact(self,t):
+        """
+        Routine to compute the exact solution at time t
+
+        Args:
+            t: current time
+
+        Returns:
+            exact solution
+        """
+        return None
+
+    def u_init(self,t0):
+
+        return df.interpolate(self.u0,self.V)
---
+++ examples/fenics_heat2d/playground.py
@@ -0,0 +1,74 @@
+
+from pySDC import CollocationClasses as collclass
+
+import numpy as np
+
+from examples.fenics_heat2d.ProblemClass import fenics_heat2d
+from pySDC.datatype_classes.fenics_mesh import fenics_mesh, rhs_fenics_mesh
+from pySDC.sweeper_classes.mass_matrix_imex import mass_matrix_imex
+import pySDC.Methods as mp
+from pySDC import Log
+from pySDC.Stats import grep_stats, sort_stats
+
+if __name__ == "__main__":
+
+    # set global logger (remove this if you do not want the output at all)
+    logger = Log.setup_custom_logger('root')
+
+    num_procs = 1
+
+    # This comes as read-in for the level class
+    lparams = {}
+    lparams['restol'] = 3E-12
+
+    sparams = {}
+    sparams['maxiter'] = 10
+
+    # This comes as read-in for the problem class
+    pparams = {}
+    pparams['alpha'] = 3
+    pparams['beta'] = 1.2
+    pparams['nvars'] = [[2,2]]
+    pparams['t0'] = 0 # ugly, but necessary to set up ProblemClass
+
+    # This comes as read-in for the transfer operations
+    # tparams = {}
+    # tparams['finter'] = True
+
+    # Fill description dictionary for easy hierarchy creation
+    description = {}
+    description['problem_class'] = fenics_heat2d
+    description['problem_params'] = pparams
+    description['dtype_u'] = fenics_mesh
+    description['dtype_f'] = rhs_fenics_mesh
+    description['collocation_class'] = collclass.CollGaussLegendre
+    description['num_nodes'] = 3
+    description['sweeper_class'] = mass_matrix_imex
+    description['level_params'] = lparams
+    # description['transfer_class'] = mesh_to_mesh_1d
+    # description['transfer_params'] = tparams
+
+    # quickly generate block of steps
+    MS = mp.generate_steps(num_procs,sparams,description)
+
+    # setup parameters "in time"
+    t0 = MS[0].levels[0].prob.t0
+    dt = 0.3
+    Tend = 1.8
+
+    # get initial values on finest level
+    P = MS[0].levels[0].prob
+    uinit = P.u_init(t0)
+
+    # call main function to get things done...
+    uend,stats = mp.run_pfasst_serial(MS,u0=uinit,t0=t0,dt=dt,Tend=Tend)
+
+    # compute exact solution and compare
+    # uex = P.u_exact(Tend)
+
+    # print('error at time %s: %s' %(Tend,np.linalg.norm(uex.values-uend.values,np.inf)/np.linalg.norm(
+    #     uex.values,np.inf)))
+
+    extract_stats = grep_stats(stats,iter=-1,type='residual')
+    sortedlist_stats = sort_stats(extract_stats,sortby='step')
+    print(extract_stats,sortedlist_stats)
