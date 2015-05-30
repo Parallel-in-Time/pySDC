7f18aa762a042039cd45e495693a83e54b508ad9
Daniel Ruprecht <daniel.ruprecht@usi.ch>
2015-05-30 15:55:31 +0200

    simplified construction of upwind matrix

--- examples/acoustic_2d_imex/buildFDMatrix.py
+++ examples/acoustic_2d_imex/buildFDMatrix.py
@@ -4,19 +4,28 @@ import scipy.linalg as la
 # Only for periodic BC because we have advection only in x direction
 def getUpwindMatrix(N, dx):
   
-  stencil = [ 2.0, -15.0,  60.0,  -20.0, -30.0, 3.0]
-  range   = [  -3,    -2,    -1,      0,     1,   2]
-  zero_index     = 3
+  stencil    = [ 2.0, -15.0,  60.0,  -20.0, -30.0, 3.0]
+  range      = [  -3,    -2,    -1,      0,     1,   2]
+  zero_index = 3
+  coeff      = 1.0/60.0
   
-  first_row      = np.zeros(N)
-  first_row[0]   = stencil[zero_index]
-  first_row[1]   = stencil[zero_index+1]
-  first_row[2]   = stencil[zero_index+2]
-  first_row[N-1] = stencil[zero_index-1]
-  first_row[N-2] = stencil[zero_index-2]
-  first_row[N-3] = stencil[zero_index-3]
+  #stencil    = [ -3.0, 20.0, -60.0, 120.0, -65.0, -12.0]
+  #range      = [   -4,   -3,    -2,    -1,     0,     1]
+  #zero_index = 4
+  #coeff      = 1.0/60.0
   
-  return sp.csc_matrix( 1.0/(60.0*dx)*la.circulant(first_row) )
+  #stencil    = [ 1.0, 0.0, -1.0]
+  #range      = [  -1,   0,    1]
+  #zero_index = 1
+  #coeff      = 1.0/2.0
+  
+  first_col = np.zeros(N)
+  first_col[0:np.size(stencil)] = stencil
+
+  # Circulant shift of coefficient column
+  first_col = np.roll(first_col, -zero_index)
+
+  return sp.csc_matrix( coeff*(1.0/dx)*la.circulant(first_col) )
 
 def getMatrix(N, dx, bc_left, bc_right):
   stencil = [1.0, -8.0, 0.0, 8.0, -1.0]
