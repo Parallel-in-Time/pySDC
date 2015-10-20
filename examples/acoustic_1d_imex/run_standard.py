30878be081ada7c606c0dec16462e21aa0acbd3a
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2015-10-20 12:13:46 +0100

    paramter fixes for multiscale initial data in 1d acoustic

--- examples/acoustic_1d_imex/run_standard.py
+++ examples/acoustic_1d_imex/run_standard.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.sparse as sp
 import scipy.sparse.linalg as LA
+import os
 from matplotlib import pyplot as plt
 from buildWave1DMatrix import getWave1DMatrix, getWave1DAdvectionMatrix
 
@@ -8,15 +9,14 @@ sigma_0 = 0.1
 k       = 7.0*2*np.pi
 x_0     = 0.75
 x_1     = 0.25
-multi_scale = 1.0
 
 nvars = 512
 cs    = 1.0
 cadv  = 0.0
 order = 4
 
-def u(x,t):
-  u0 = np.exp(-np.square( np.mod( mesh- cs*t, 1.0 ) -x_0 )/(sigma_0*sigma_0)) + multi_scale*np.exp(-np.square( np.mod( mesh -cs*t, 1.0 ) -x_1 )/(sigma_0*sigma_0))*np.cos(k*( np.mod( mesh-cs*t, 1.0 ))/sigma_0)
+def u(x,t, multiscale):
+  u0 = np.exp(-np.square( np.mod( mesh- cs*t, 1.0 ) -x_0 )/(sigma_0*sigma_0)) + multiscale*np.exp(-np.square( np.mod( mesh -cs*t, 1.0 ) -x_1 )/(sigma_0*sigma_0))*np.cos(k*( np.mod( mesh-cs*t, 1.0 ))/sigma_0)
   p0 = u0
   return u0, p0
 
@@ -38,7 +38,7 @@ alpha    = 0.5
 M_trap   = Id - alpha*dt*(A+Dx)
 B_trap   = Id + (1-alpha)*dt*(A+Dx)
  
-u0, p0 = u(mesh, 0.0)
+u0, p0 = u(mesh, 0.0, 1.0)
 y0_ie  = np.concatenate( (u0, p0) )
 y0_tp  = y0_ie
 
@@ -65,23 +65,30 @@ for i in range(0,Nsteps):
   unew_ie, pnew_ie = np.split(ynew_ie, 2)
   unew_tp, pnew_tp = np.split(ynew_tp, 2)
   unew_bdf, pnew_bdf = np.split(ynew_bdf, 2)
-  uex, pex = u(mesh, float(i+1)*dt)
+  uex, pex = u(mesh, float(i+1)*dt, 0.0)
 
   fig.gca().clear()
   #plt.plot(mesh, pnew_bdf, 'b', label='BDF-2')
   plt.plot(mesh, pnew_tp, 'r', label='Trapezoidal')
-  plt.plot(mesh, pex, 'k', label='Exact')
+  plt.plot(mesh, pex, 'k', label='Slow Mode')
   fig.gca().set_xlim([0, 1.0])
   fig.gca().set_ylim([-0.5, 1.1])
   fig.gca().legend(loc=3)
   fig.gca().grid()
-  plt.draw()
+  #plt.draw()
   plt.pause(0.0001)  
   #if i==0:
-  #  plt.gcf().savefig('initial.pdf', bbox_inches='tight')
+    #plt.gcf().savefig('initial.pdf', bbox_inches='tight')
+  filename = 'images/standard'+"%03d" % i
+  plt.gcf().savefig(filename+'.png', bbox_inches='tight')
+  os.system('convert -quality 100 '+filename+'.png '+filename+'.jpeg')
+  os.system('rm '+filename+'.png')
+
   y0_ie   = ynew_ie
   y0_tp   = ynew_tp
   ym1_bdf = y0_bdf
   y0_bdf  = ynew_bdf
 #plt.show()
-#plt.gcf().savefig('final.pdf', bbox_inches='tight')
+#lt.gcf().savefig('final.pdf', bbox_inches='tight')
+os.system('ffmpeg -r 25 -i images/standard-%03d.jpeg -vcodec libx264 -crf 25 movie.avi')
+
