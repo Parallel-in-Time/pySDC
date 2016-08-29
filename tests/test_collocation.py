b5ac3e30a6c12d670c6e4c09195dca7259278b63
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2016-08-29 16:31:59 +0200

    equidistant nodes only tested up to M=12. For larger values of M, tests start to fail because of round-off errors

--- tests/test_collocation.py
+++ tests/test_collocation.py
@@ -8,7 +8,7 @@ import numpy as np
 t_start  = np.random.rand(1)*0.2
 t_end    = 0.8 + np.random.rand(1)*0.2
 
-classes  = [ ["CollGaussLegendre", 2, 12], ["CollGaussLobatto", 2, 12], ["CollGaussRadau_Right", 2, 12], ["CollGaussRadau_Left", 2, 12], ["EquidistantInner", 2, 15], ["EquidistantNoLeft", 2, 15] ]
+classes  = [ ["CollGaussLegendre", 2, 12], ["CollGaussLobatto", 2, 12], ["CollGaussRadau_Right", 2, 12], ["CollGaussRadau_Left", 2, 12], ["EquidistantInner", 2, 12], ["EquidistantNoLeft", 2, 12] ]
 
 class TestCollocation:
 
