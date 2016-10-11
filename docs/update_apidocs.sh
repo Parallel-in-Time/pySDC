b1a31732542815c9b40e3827fc2d744f89dec138
Robert Speck <r.speck@fz-juelich.de>
2016-10-11 16:55:30 +0200

    renamed doc dir

---
+++ docs/update_apidocs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Assuring we are running in the project's root
+[[ -d "${PWD}/docs" && "./docs/update_apidocs.sh" == "$0" ]] ||
+    {
+        echo "ERROR: You must be in the project root."
+        exit 1
+    }
+
+SPHINX_APIDOC="`which sphinx-apidoc`"
+[[ -x "$SPHINX_APIDOC" ]] ||
+    {
+        echo "ERROR: sphinx-apidoc not found."
+        exit 1
+    }
+
+echo "removing existing .rst files ..."
+rm ${PWD}/docs/source/pySDC/*.rst
+rm ${PWD}/docs/source/implementations/*.rst
+
+echo ""
+echo "generating new .rst files ..."
+${SPHINX_APIDOC} -o docs/source/pySDC pySDC --force
+${SPHINX_APIDOC} -o docs/source/implementations implementations --force
