b278b8e95be390fea74c32c77cfdaa842ad105ba
Robert Speck <r.speck@fz-juelich.de>
2016-10-24 14:45:04 +0200

    fixed test6

--- tests/test_tutorials/test_step_6.py
+++ tests/test_tutorials/test_step_6.py
@@ -6,14 +6,14 @@ from tutorial.step_6.B_classic_vs_multigrid_controller import main as main_B
 from tutorial.step_6.C_multistep_SDC import main as main_C
 from tutorial.step_6.D_MPI_parallelization import main as main_D
 
-# def test_A():
-#     main_A()
-#
-# def test_B():
-#     main_B()
-# #
-# def test_C():
-#     main_C()
+def test_A():
+    main_A()
+
+def test_B():
+    main_B()
+
+def test_C():
+    main_C()
 
 def test_D():
     cwd = 'tutorial/step_6'
