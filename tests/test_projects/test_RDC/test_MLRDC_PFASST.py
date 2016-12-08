270f13441052bc4a4d1fda7b0de36e3448204381
Robert Speck <r.speck@fz-juelich.de>
2016-12-08 10:40:09 +0100

    fixes

--- tests/test_projects/test_RDC/test_MLRDC_PFASST.py
+++ tests/test_projects/test_RDC/test_MLRDC_PFASST.py
@@ -4,5 +4,5 @@ def test_RDC_flavors():
     results = run_RDC(cwd='projects/RDC/')
 
     for item in results:
-        assert item[0] < 9E-06, 'Error too high, got %s' % item
-        assert item[1] < 12, 'Iterations too high, got %s' % item
+        assert item[0] < 9E-06, 'Error too high, got %s' % item[0]
+        assert item[1] < 12.1, 'Iterations too high, got %s' % item[1]
