ff903462c4b7da06b8eb038a8959cb13748d7543
Robert Speck <r.speck@fz-juelich.de>
2018-03-09 16:40:43 +0900

    added stencil and documentation

--- pySDC/projects/matrixPFASST/dispersion_relation.py
+++ pySDC/projects/matrixPFASST/dispersion_relation.py
@@ -10,13 +10,30 @@ from pySDC.implementations.transfer_classes.TransferMesh_NoCoarse import mesh_to
 from pySDC.projects.matrixPFASST.allinclusive_matrix_nonMPI import allinclusive_matrix_nonMPI
 
 
-# computes the frequency omega = 1j*log(R)
 def solve_omega(R):
-    return 1j * (np.log(abs(R)) + 1j * np.angle(R))
+    """
+    Computes the frequency omega = 1j*log(R)
+
+    Args:
+        R (complex): stability function
+
+    Returns:
+        frequency omega
+    """
+    return 1j * (np.log(abs(R)) + 1j * np.angle([R]))
 
 
-# finds all roots of the stability function
 def findroots(R, n):
+    """
+    Finds all roots of the stability function
+
+    Args:
+        R (complex): stability function
+        n (float): number of time-steps/processors and/or Tend
+
+    Returns:
+        roots of the stability function
+    """
     assert abs(n - float(int(n))) < 1e-14, "n must be an integer or a float equal to an integer"
     p = np.zeros(int(n) + 1, dtype='complex')
     p[-1] = -R
@@ -24,17 +41,25 @@ def findroots(R, n):
     return np.roots(p)
 
 
-# normalises the stability function of Parareal from [0,Tend] to [0,1]
-# by computing all Tend=P roots and then selecting the one that is closest to the given
-# target angle
-def normalise(R, T, target):
+def normalize(R, T, target):
+    """
+    normalizes the stability function from [0,Tend] to [0,1]
+
+    Args:
+        R (complex): stability function
+        T (float): number of time-steps/processors and/or Tend
+        target (complex): target angle
+
+    Returns:
+        root with minimal distance to previous one
+    """
     roots = findroots(R, T)
 
     # make sure all computed values are actually roots
     for x in roots:
         assert abs(x ** T - R) < 1e-3, ("Element in roots not a proper root: err=%5.3e" % abs(x ** T - R))
 
-    # find root that minimises distance to target angle
+    # find root that minimizes distance to target angle
     minind = np.argmin(abs(np.angle(roots) - target))
     return roots[minind]
 
@@ -43,7 +68,8 @@ def scalar_equation_setup():
     """
     Setup routine for the test equation
 
-    Args:
+    Returns:
+        description and controller parameters
     """
     # initialize level parameters
     level_params = dict()
@@ -64,7 +90,7 @@ def scalar_equation_setup():
 
     # initialize step parameters
     step_params = dict()
-    step_params['maxiter'] = 4
+    step_params['maxiter'] = None
 
     # initialize controller parameters
     controller_params = dict()
@@ -87,17 +113,33 @@ def scalar_equation_setup():
     return description, controller_params
 
 
-def compute_dispersion_relation(npoints=None, f=None):
+def compute_dispersion_relation(npoints=None, dx=None, c=1.0):
+    """
+    Computes the discrete dispersion relation for PFASST
 
+    Args:
+        npoints: number of samples over [0,pi]
+        dx (float): virtual mesh size for stencil symbols
+        c (float): advection speed
+
+    Returns:
+        iteration counts, wave numbers, phases and amplification factors,
+    """
     num_procs = 16
     Tend = float(num_procs)
 
-    deltas = -1j * np.linspace(0, np.pi, npoints + 1, endpoint=False)[1:]
+    # set up factors for linear ODE u_t = delta u here
+    kappas = np.linspace(0, np.pi, npoints + 1, endpoint=False)[1:]
+    if dx is None:
+        deltas = -1j * kappas
+    else:
+        # upwind differences
+        deltas = -c * (1.0 - np.exp(-1j * kappas * dx)) / dx
+    # iteration counts to check for PFASST
     niters = [1, 2, 3]
 
     phase = np.zeros((len(niters), npoints), dtype=np.complex64)
     amp_factor = np.zeros((len(niters), npoints), dtype=np.complex64)
-
 
     for idx, niter in enumerate(niters):
 
@@ -105,6 +147,7 @@ def compute_dispersion_relation(npoints=None, f=None):
 
         for idy, delta in enumerate(deltas):
 
+            # set parameters
             description, controller_params = scalar_equation_setup()
             description['problem_params']['lambdas'] = [[delta]]  # pass problem parameters
             description['step_params']['maxiter'] = niter
@@ -116,66 +159,73 @@ def compute_dispersion_relation(npoints=None, f=None):
             # build propagation matrix using the prescribed number of iterations (or any other, if needed)
             prop = controller.build_propagation_matrix(niter=description['step_params']['maxiter'])
 
-            pfasst_norm = normalise(prop[0, 0], Tend, target_angle)
+            # normalize propagation matrix
+            pfasst_norm = normalize(prop[0, 0], Tend, target_angle)
 
             # Make sure that stab_norm*dt = stab
             err = abs(pfasst_norm ** Tend - prop[0, 0])
             if err > 1e-10:
-                print("WARNING: power of norm. update does not match update over full length of time. error %5.3e" % err)
+                print("WARNING: power of norm. update does not match update over full time length, got %5.3e" % err)
 
             target_angle = np.angle(pfasst_norm)
 
             sol_pfasst = solve_omega(pfasst_norm)
-            phase[idx, idy] = sol_pfasst.real / -delta.imag
+            phase[idx, idy] = sol_pfasst.real / kappas[idy]
             amp_factor[idx, idy] = np.exp(sol_pfasst.imag)
 
-    return niters, deltas, phase, amp_factor
+    return niters, kappas, phase, amp_factor
 
 
-def plot_dispersion_relations(niters, deltas, phase, amp_factor):
+def plot_dispersion_relations(niters, kappas, phase, amp_factor, c):
+    """
+    Plots amplification factor and phases
 
+    Args:
+        niters: iteration counts
+        kappas: wave numbers
+        phase: phases
+        amp_factor: amplification factors
+        c: advection speed
+    """
     colors = ['r', 'b', 'g']
     rcParams['figure.figsize'] = 3.54, 3.54
     fs = 8
+
+    # Phases
     fig = plt.figure()
     for idx, niter in enumerate(niters):
-        plt.plot(-deltas.imag, phase[idx, :], '--', color=colors[idx], linewidth=1.5, label='PFASST, k=%i' % niter)
-    plt.plot(-deltas.imag, 0 * deltas + 1, '-', color='k', linewidth=0.5, label='1')
+        plt.plot(kappas, phase[idx, :], '--', color=colors[idx], linewidth=1.5, label='PFASST, k=%i' % niter)
+    plt.plot(kappas, 0 * kappas + c, '-', color='k', linewidth=0.5, label='1')
     plt.xlabel('Wave number', fontsize=fs, labelpad=0.25)
     plt.ylabel('Phase speed', fontsize=fs, labelpad=0.5)
-    plt.xlim([-deltas[0].imag, -deltas[-1].imag])
-    plt.ylim([0.0, 1.2])
+    plt.xlim([kappas[0], kappas[-1]])
+    plt.ylim([0.0, 1.2 * c])
     fig.gca().tick_params(axis='both', labelsize=fs)
     plt.legend(loc='lower left', fontsize=fs, prop={'size': fs - 2})
     plt.xticks([0, 1, 2, 3], fontsize=fs)
-    # plt.show()
     filename = 'pfasst-dispersion-phase.pdf'
     plt.gcf().savefig(filename, bbox_inches='tight')
-    # call(["pdfcrop", filename, filename])
 
+    # Amplification factors
     fig = plt.figure()
     for idx, niter in enumerate(niters):
-        plt.plot(-deltas.imag, amp_factor[idx, :], '--', color=colors[idx], linewidth=1.5, label='PFASST, k=%i' % niter)
-    plt.plot(-deltas.imag, 0 * deltas + 1, '-', color='k', linewidth=0.5, label='1')
+        plt.plot(kappas, amp_factor[idx, :], '--', color=colors[idx], linewidth=1.5, label='PFASST, k=%i' % niter)
+    plt.plot(kappas, 0 * kappas + 1, '-', color='k', linewidth=0.5, label='1')
     plt.xlabel('Wave number', fontsize=fs, labelpad=0.25)
     plt.ylabel('Amplification factor', fontsize=fs, labelpad=0.5)
     fig.gca().tick_params(axis='both', labelsize=fs)
-    plt.xlim([-deltas[0].imag, -deltas[-1].imag])
+    plt.xlim([kappas[0], kappas[-1]])
     plt.legend(loc='lower left', fontsize=fs, prop={'size': fs - 2})
     plt.gca().set_ylim([0.0, 1.2])
     plt.xticks([0, 1, 2, 3], fontsize=fs)
-    # plt.show()
     filename = 'pfasst-dispersion-ampf.pdf'
     plt.gcf().savefig(filename, bbox_inches='tight')
-    # call(["pdfcrop", filename, filename])
-#
+
 
 def main():
-
-    f = open('dispersion_relation.txt', 'w')
-    niters, deltas, phase, amp_factor = compute_dispersion_relation(npoints=32, f=f)
-    plot_dispersion_relations(niters, deltas, phase, amp_factor)
-    f.close()
+    c = 1e1
+    niters, kappas, phase, amp_factor = compute_dispersion_relation(npoints=32, dx=1.0/128.0, c=c)
+    plot_dispersion_relations(niters, kappas, phase, amp_factor, c)
 
 
 if __name__ == "__main__":
