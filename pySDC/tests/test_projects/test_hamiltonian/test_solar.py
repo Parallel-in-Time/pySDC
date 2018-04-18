14be9bec25afcf5e997ebd560cf91161e6c15a36
Robert Speck <r.speck@fz-juelich.de>
2018-04-18 12:19:55 +0200

    fixing path issue

--- pySDC/tests/test_projects/test_hamiltonian/test_solar.py
+++ pySDC/tests/test_projects/test_hamiltonian/test_solar.py
@@ -2,4 +2,4 @@ from pySDC.projects.Hamiltonian.solar_system import show_results
 
 def test_main():
     prob = 'outer_solar_system'
-    show_results(prob)
+    show_results(prob, cwd='pySDC/projects/Hamiltonian/')
