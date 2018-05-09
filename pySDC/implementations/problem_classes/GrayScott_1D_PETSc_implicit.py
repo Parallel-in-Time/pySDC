79102bf1d47dda864cd0d97abc845294f7b73f4c
Robert Speck <r.speck@fz-juelich.de>
2018-05-09 08:22:28 +0200

    WIP nonlinear

--- pySDC/implementations/problem_classes/GrayScott_1D_PETSc_implicit.py
+++ pySDC/implementations/problem_classes/GrayScott_1D_PETSc_implicit.py
@@ -1,23 +1,17 @@
 from __future__ import division
-
+import dolfin as df
 import numpy as np
-import scipy.sparse as sp
-
+import random
 from petsc4py import PETSc
 
 from pySDC.core.Problem import ptype
-from pySDC.core.Errors import ParameterError, ProblemError
+from pySDC.core.Errors import ParameterError
 
 
 # noinspection PyUnusedLocal
-class heat2d_petsc_forced(ptype):
+class petsc_grayscott(ptype):
     """
-    Example implementing the unforced 2D heat equation with periodic BCs in [0,1]^2,
-    discretized using central finite differences
 
-    Attributes:
-        A: second-order FD discretization of the 2D laplace operator
-        dx: distance between two spatial nodes (here: being the same in both dimensions)
     """
 
     def __init__(self, problem_params, dtype_u, dtype_f):
@@ -25,37 +19,30 @@ class heat2d_petsc_forced(ptype):
         Initialization routine
 
         Args:
-            problem_params (dict): custom parameters for the example
-            dtype_u: mesh data type (will be passed parent class)
-            dtype_f: mesh data type (will be passed parent class)
+            problem_params: custom parameters for the example
+            dtype_u: particle data type (will be passed parent class)
+            dtype_f: acceleration data type (will be passed parent class)
         """
 
+        # define the Dirichlet boundary
+        def Boundary(x, on_boundary):
+            return on_boundary
+
         # these parameters will be used later, so assert their existence
-
-        if 'comm' not in problem_params:
-            problem_params['comm'] = PETSc.COMM_WORLD
-        if 'sol_tol' not in problem_params:
-            problem_params['sol_tol'] = 1E-10
-        if 'sol_maxiter' not in problem_params:
-            problem_params['sol_maxiter'] = None
-
-        essential_keys = ['nvars', 'nu', 'freq', 'comm']
+        essential_keys = ['nvars', 'Du', 'Dv', 'A', 'B']
         for key in essential_keys:
             if key not in problem_params:
                 msg = 'need %s to instantiate problem, only got %s' % (key, str(problem_params.keys()))
                 raise ParameterError(msg)
 
-        # make sure parameters have the correct form
-        if len(problem_params['nvars']) != 2:
-            raise ProblemError('this is a 2d example, got %s' % problem_params['nvars'])
-
         da = PETSc.DMDA().create([problem_params['nvars'][0], problem_params['nvars'][1]], stencil_width=1,
                                  comm=problem_params['comm'])
 
         # invoke super init, passing number of dofs, dtype_u and dtype_f
-        super(heat2d_petsc_forced, self).__init__(init=da, dtype_u=dtype_u, dtype_f=dtype_f, params=problem_params)
+        super(petsc_grayscott, self).__init__(init=da, dtype_u=dtype_u, dtype_f=dtype_f, params=problem_params)
 
-        # compute dx, dy and get local ranges
+
+       # compute dx, dy and get local ranges
         self.dx = 1.0 / (self.params.nvars[0] - 1)
         self.dy = 1.0 / (self.params.nvars[1] - 1)
         (self.xs, self.xe), (self.ys, self.ye) = self.init.getRanges()
@@ -63,6 +50,7 @@ class heat2d_petsc_forced(ptype):
         # compute discretization matrix A and identity
         self.A = self.__get_A()
         self.Id = self.__get_Id()
+        self.localX = self.init.createLocalVec()
 
         # setup solver
         self.ksp = PETSc.KSP()
@@ -99,16 +87,32 @@ class heat2d_petsc_forced(ptype):
                 if i == 0 or j == 0 or i == mx - 1 or j == my - 1:
                     A.setValueStencil(row, row, 1.0)
                 else:
-                    diag = self.params.nu * (-2.0 / self.dx ** 2 - 2.0 / self.dy ** 2)
+                    diag = self.params.Du * (-2.0 / self.dx ** 2 - 2.0 / self.dy ** 2)
                     for index, value in [
-                        ((i, j - 1), self.params.nu / self.dy ** 2),
-                        ((i - 1, j), self.params.nu / self.dx ** 2),
+                        ((i, j - 1), self.params.Du / self.dy ** 2),
+                        ((i - 1, j), self.params.Du / self.dx ** 2),
                         ((i, j), diag),
-                        ((i + 1, j), self.params.nu / self.dx ** 2),
-                        ((i, j + 1), self.params.nu / self.dy ** 2),
+                        ((i + 1, j), self.params.Du / self.dx ** 2),
+                        ((i, j + 1), self.params.Du / self.dy ** 2),
                     ]:
                         col.index = index
                         col.field = 0
+                        A.setValueStencil(row, col, value)
+                row.index = (i, j)
+                row.field = 1
+                if i == 0 or j == 0 or i == mx - 1 or j == my - 1:
+                    A.setValueStencil(row, row, 1.0)
+                else:
+                    diag = self.params.Dv * (-2.0 / self.dx ** 2 - 2.0 / self.dy ** 2)
+                    for index, value in [
+                        ((i, j - 1), self.params.Dv / self.dy ** 2),
+                        ((i - 1, j), self.params.Dv / self.dx ** 2),
+                        ((i, j), diag),
+                        ((i + 1, j), self.params.Dv / self.dx ** 2),
+                        ((i, j + 1), self.params.Dv / self.dy ** 2),
+                    ]:
+                        col.index = index
+                        col.field = 1
                         A.setValueStencil(row, col, value)
         A.assemble()
 
@@ -135,15 +139,49 @@ class heat2d_petsc_forced(ptype):
         (xs, xe), (ys, ye) = self.init.getRanges()
         for j in range(ys, ye):
             for i in range(xs, xe):
-                row.index = (i, j)
-                row.field = 0
-                col.index = (i, j)
-                col.field = 0
-                Id.setValueStencil(row, col, 1.0)
+                for indx in [0, 1]:
+                    row.index = (i, j)
+                    row.field = indx
+                    Id.setValueStencil(row, col, 1.0)
 
         Id.assemble()
 
         return Id
+
+    def __form_Jacobian(self, sens, X, J, P):
+        self.init.globalToLocal(X, self.localX)
+        x = self.init.getVecArray(self.localX)
+        mx, my = self.init.getSizes()
+        P.zeroEntries()
+        row = PETSc.Mat.Stencil()
+        col = PETSc.Mat.Stencil()
+        (xs, xe), (ys, ye) = self.init.getRanges()
+        for j in range(ys, ye):
+            for i in range(xs, xe):
+                row.index = (i,j)
+                row.field = 0
+                if (i==0    or j==0    or k==0 or
+                    i==mx-1 or j==my-1 or k==mz-1):
+                    P.setValueStencil(row, row, 1.0)
+                else:
+                    u = x[i,j,k]
+                    diag = (2*(hyhzdhx+hxhzdhy+hxhydhz)
+                            - lambda_*exp(u)*hxhyhz)
+                    for index, value in [
+                        ((i,j,k-1), -hxhydhz),
+                        ((i,j-1,k), -hxhzdhy),
+                        ((i-1,j,k), -hyhzdhx),
+                        ((i, j, k), diag),
+                        ((i+1,j,k), -hyhzdhx),
+                        ((i,j+1,k), -hxhzdhy),
+                        ((i,j,k+1), -hxhydhz),
+                        ]:
+                        col.index = index
+                        col.field = 0
+                        P.setValueStencil(row, col, value)
+        P.assemble()
+        if J != P: J.assemble() # matrix-free operator
+        return PETSc.Mat.Structure.SAME_NONZERO_PATTERN
 
     def eval_f(self, u, t):
         """
@@ -204,7 +242,9 @@ class heat2d_petsc_forced(ptype):
         xa = self.init.getVecArray(me.values)
         for i in range(self.xs, self.xe):
             for j in range(self.ys, self.ye):
-                xa[i, j] = np.sin(np.pi * self.params.freq * i * self.dx) * \
-                    np.sin(np.pi * self.params.freq * j * self.dy) * np.cos(t)
+                xa[i, j, 0] = 1.0 - 0.5 * np.power(np.sin(np.pi * i * self.dx / 100) *
+                                                   np.sin(np.pi * j * self.dy / 100), 100)
+                xa[i, j, 1] = 0.25 * np.power(np.sin(np.pi * i * self.dx / 100) *
+                                              np.sin(np.pi * j * self.dy / 100), 100)
 
         return me
