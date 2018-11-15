66a4bfd45da76592544eebfd6d6298e678b68694
Robert Speck <r.speck@fz-juelich.de>
2018-11-15 12:56:52 +0100

    debugging...........

--- pySDC/tutorial/step_6/C_MPI_parallelization.py
+++ pySDC/tutorial/step_6/C_MPI_parallelization.py
@@ -24,7 +24,7 @@ def main(cwd):
     # run code with different number of MPI processes
     for num_procs in num_procs_list:
         print('Running code with %2i processes...' % num_procs)
-        cmd = ('mpirun -np ' + str(num_procs) + ' python playground_parallelization.py ../../../' + fname).split()
+        cmd = ('mpirun -np ' + str(num_procs) + ' --oversubscribe python playground_parallelization.py ../../../' + fname).split()
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=my_env, cwd=cwd)
         p.wait()
         assert p.returncode == 0, 'ERROR: did not get return code 0, got %s with %2i processes' % \
@@ -41,7 +41,7 @@ def main(cwd):
     # run code with different number of MPI processes
     for num_procs in num_procs_list:
         print('Running code with %2i processes...' % num_procs)
-        cmd = ('mpirun -np ' + str(num_procs) + ' python playground_parallelization.py ../../../' + fname).split()
+        cmd = ('mpirun -np ' + str(num_procs) + ' --oversubscribe python playground_parallelization.py ../../../' + fname).split()
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=my_env, cwd=cwd)
         p.wait()
         assert p.returncode == 0, 'ERROR: did not get return code 0, got %s with %2i processes' % \
