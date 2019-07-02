67b09252053556c740d956fa468b4f8aedea6b95
Robert Speck <r.speck@fz-juelich.de>
2019-07-02 12:57:47 +0200

    minor fixes

--- pySDC/core/Controller.py
+++ pySDC/core/Controller.py
@@ -196,6 +196,8 @@ class controller(object):
         # get residual and check against prescribed tolerance (plus check number of iterations
         res = L.status.residual
         converged = S.status.iter >= S.params.maxiter or res <= L.params.restol or S.status.force_done
+        if converged is None:
+            converged = False
 
         return converged
 
--- pySDC/implementations/sweeper_classes/imex_1st_order.py
+++ pySDC/implementations/sweeper_classes/imex_1st_order.py
@@ -50,10 +50,10 @@ class imex_1st_order(sweeper):
 
         # integrate RHS over all collocation nodes
         for m in range(1, self.coll.num_nodes + 1):
+            me.append(L.dt * self.coll.Qmat[m, 1] * (L.f[1].impl + L.f[1].expl))
             # new instance of dtype_u, initialize values with 0
-            me.append(P.dtype_u(P.init, val=0.0))
-            for j in range(1, self.coll.num_nodes + 1):
-                me[-1] += L.dt * self.coll.Qmat[m, j] * (L.f[j].impl + L.f[j].expl)
+            for j in range(2, self.coll.num_nodes + 1):
+                me[m - 1] += L.dt * self.coll.Qmat[m, j] * (L.f[j].impl + L.f[j].expl)
 
         return me
 
