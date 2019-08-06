cf0349899afa04f0cef44ef2f645847a847e334a
Robert Speck <r.speck@fz-juelich.de>
2019-08-06 09:21:00 +0200

    added simple ND heat equation solver to play with

---
+++ pySDC/playgrounds/compression/HookClass_error_output.py
@@ -0,0 +1,51 @@
+from pySDC.core.Hooks import hooks
+
+
+class error_output(hooks):
+    """
+    Hook class to add output of error
+    """
+
+    def post_iteration(self, step, level_number):
+        """
+        Default routine called after each iteration
+        Args:
+            step: the current step
+            level_number: the current level number
+        """
+
+        super(error_output, self).post_iteration(step, level_number)
+
+        # some abbreviations
+        L = step.levels[level_number]
+        P = L.prob
+
+        L.sweep.compute_end_point()
+
+        uex = P.u_exact(step.time + step.dt)
+        err = abs(uex - L.uend)
+
+        self.add_to_stats(process=step.status.slot, time=L.time + L.dt, level=L.level_index, iter=step.status.iter,
+                          sweep=L.status.sweep, type='error_after_iter', value=err)
+
+    def post_step(self, step, level_number):
+        """
+        Default routine called after each step
+        Args:
+            step: the current step
+            level_number: the current level number
+        """
+
+        super(error_output, self).post_step(step, level_number)
+
+        # some abbreviations
+        L = step.levels[level_number]
+        P = L.prob
+
+        L.sweep.compute_end_point()
+
+        uex = P.u_exact(step.time + step.dt)
+        err = abs(uex - L.uend)
+
+        self.add_to_stats(process=step.status.slot, time=L.time + L.dt, level=L.level_index, iter=step.status.iter,
+                          sweep=L.status.sweep, type='error_after_step', value=err)
