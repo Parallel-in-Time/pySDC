09de5d3b7e0c327700e08ab53f9430cefe107c9d
Robert Speck <r.speck@fz-juelich.de>
2019-08-08 14:05:03 +0200

    adding advection and auzinger

--- pySDC/implementations/controller_classes/controller_nonMPI.py
+++ pySDC/implementations/controller_classes/controller_nonMPI.py
@@ -98,8 +98,9 @@ class controller_nonMPI(controller):
                 Ltilde_loc = min(diff_new / S.status.diff_old_loc, 0.9)
                 S.status.diff_old_loc = diff_new
                 alpha = 1 / (1 - Ltilde_loc) * S.status.diff_first_loc
-                Kest_loc = np.log(S.params.err_tol / alpha) / np.log(Ltilde_loc)
-                print(f'LOCAL: {L.time}, {S.status.iter}: {int(np.ceil(Kest_loc))}, {Ltilde_loc}, {L.status.residual}')
+                Kest_loc = np.log(S.params.err_tol / alpha) / np.log(Ltilde_loc) * 1.05  # Safety factor!
+                print(f'LOCAL: {L.time:8.4f}, {S.status.iter}: {int(np.ceil(Kest_loc))}, {Ltilde_loc:8.6e}, '
+                      f'{Kest_loc:8.6e}, {Ltilde_loc ** S.status.iter * alpha:8.6e}')
                 # You should not stop prematurely on earlier steps, since later steps may need more accuracy to reach
                 # the tolerance themselves. The final Kest_loc is the one that counts.
                 # if np.ceil(Kest_loc) <= S.status.iter:
--- pySDC/playgrounds/compression/HookClass_error_output.py
+++ pySDC/playgrounds/compression/HookClass_error_output.py
@@ -1,10 +1,15 @@
 from pySDC.core.Hooks import hooks
-
+from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
 
 class error_output(hooks):
     """
     Hook class to add output of error
     """
+
+    def __init__(self):
+        super(error_output, self).__init__()
+        self.uex = None
+
 
     # def post_iteration(self, step, level_number):
     #     """
@@ -28,6 +33,30 @@ class error_output(hooks):
     #     self.add_to_stats(process=step.status.slot, time=L.time + L.dt, level=L.level_index, iter=step.status.iter,
     #                       sweep=L.status.sweep, type='error_after_iter', value=err)
 
+    def pre_step(self, step, level_number):
+        """
+        Default routine called before each step
+        Args:
+            step: the current step
+            level_number: the current level number
+        """
+        super(error_output, self).pre_step(step, level_number)
+
+        L = step.levels[level_number]
+
+        description = step.params.description
+        description['level_params']['restol'] = 1E-14
+        # description['problem_params']['nvars'] = (64,)
+        description['problem_params']['direct_solver'] = True
+
+        controller_params = step.params.controller_params
+        del controller_params['hook_class']
+        controller_params['use_iteration_estimator'] = False
+        controller_params['logger_level'] = 90
+
+        controller = controller_nonMPI(num_procs=1, description=description, controller_params=controller_params)
+        self.uex, _ = controller.run(u0=L.u[0], t0=L.time, Tend=L.time + L.dt)
+
     def post_step(self, step, level_number):
         """
         Default routine called after each step
@@ -44,8 +73,11 @@ class error_output(hooks):
 
         L.sweep.compute_end_point()
 
-        uex = P.u_exact(step.time + step.dt)
-        err = abs(uex - L.uend)
+        upde = P.u_exact(step.time + step.dt)
+        pde_err = abs(upde - L.uend)
+        coll_err = abs(self.uex - L.uend)
 
         self.add_to_stats(process=step.status.slot, time=L.time + L.dt, level=L.level_index, iter=step.status.iter,
-                          sweep=L.status.sweep, type='error_after_step', value=err)
+                          sweep=L.status.sweep, type='PDE_error_after_step', value=pde_err)
+        self.add_to_stats(process=step.status.slot, time=L.time + L.dt, level=L.level_index, iter=step.status.iter,
+                          sweep=L.status.sweep, type='coll_error_after_step', value=coll_err)
