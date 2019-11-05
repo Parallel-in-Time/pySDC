1f8642565d916db4f1586bf8da2b818d0fcb1e4c
Robert Speck <r.speck@fz-juelich.de>
2019-11-05 12:50:19 +0100

    mass matrix magic plots

--- pySDC/playgrounds/FEniCS/HookClass_FEniCS_output.py
+++ pySDC/playgrounds/FEniCS/HookClass_FEniCS_output.py
@@ -10,39 +10,57 @@ class fenics_output(hooks):
     Hook class to add output to FEniCS runs
     """
 
-    def pre_run(self, step, level_number):
-        """
-        Overwrite default routine called before time-loop starts
+    # def pre_run(self, step, level_number):
+    #     """
+    #     Overwrite default routine called before time-loop starts
+    #
+    #     Args:
+    #         step: the current step
+    #         level_number: the current level number
+    #     """
+    #     super(fenics_output, self).pre_run(step, level_number)
+    #
+    #     # some abbreviations
+    #     L = step.levels[level_number]
+    #
+    #     v = L.u[0].values
+    #     v.rename('func', 'label')
+    #
+    #     file << v
 
-        Args:
-            step: the current step
-            level_number: the current level number
-        """
-        super(fenics_output, self).pre_run(step, level_number)
+    def post_iteration(self, step, level_number):
 
-        # some abbreviations
-        L = step.levels[level_number]
-
-        v = L.u[0].values
-        v.rename('func', 'label')
-
-        file << v
-
-    def post_step(self, step, level_number):
-        """
-        Default routine called after each iteration
-        Args:
-            step: the current step
-            level_number: the current level number
-        """
-
-        super(fenics_output, self).post_step(step, level_number)
+        super(fenics_output, self).post_iteration(step, level_number)
 
         # some abbreviations
         L = step.levels[level_number]
 
-        # u1,u2 = df.split(L.uend.values)
-        v = L.uend.values
-        v.rename('func', 'label')
+        uex = L.prob.u_exact(L.time + L.dt)
 
-        file << v
+        err = abs(uex - L.u[-1]) / abs(uex)
+
+        self.add_to_stats(process=step.status.slot, time=L.time, level=L.level_index, iter=step.status.iter,
+                          sweep=L.status.sweep, type='error', value=err)
+
+        self.add_to_stats(process=step.status.slot, time=L.time, level=L.level_index, iter=step.status.iter,
+                          sweep=L.status.sweep, type='residual', value=L.status.residual / abs(L.u[0]))
+
+
+    # def post_step(self, step, level_number):
+    #     """
+    #     Default routine called after each iteration
+    #     Args:
+    #         step: the current step
+    #         level_number: the current level number
+    #     """
+    #
+    #     super(fenics_output, self).post_step(step, level_number)
+    #
+    #     # some abbreviations
+    #     L = step.levels[level_number]
+    #
+    #     # u1,u2 = df.split(L.uend.values)
+    #     v = L.uend.values
+    #     v.rename('func', 'label')
+    #
+    #     file << v
