e618e89bc2c68a13fa8416bcb5e3d79dbef6d0ae
Robert Speck <r.speck@fz-juelich.de>
2019-11-20 14:04:38 +0100

    testing new FPUT

--- pySDC/implementations/problem_classes/FermiPastaUlamTsingou.py
+++ pySDC/implementations/problem_classes/FermiPastaUlamTsingou.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numba import jit
 
 from pySDC.core.Errors import ParameterError
 from pySDC.core.Problem import ptype
@@ -32,12 +31,7 @@ class fermi_pasta_ulam_tsingou(ptype):
         # invoke super init, passing nparts, dtype_u and dtype_f
         super(fermi_pasta_ulam_tsingou, self).__init__(problem_params['npart'], dtype_u, dtype_f, problem_params)
 
-    @staticmethod
-    @jit
-    def fast_acceleration(N, alpha, pos, accel):
-        for n in range(1, N - 1):
-            accel[n - 1] = pos[n - 1] - 2.0 * pos[n] + pos[n + 1] + \
-                alpha * ((pos[n + 1] - pos[n]) ** 2 - (pos[n] - pos[n - 1]) ** 2)
+        self.xvalues = np.array([i / (self.params.npart - 1) for i in range(self.params.npart)])
 
     def eval_f(self, u, t):
         """
@@ -51,7 +45,9 @@ class fermi_pasta_ulam_tsingou(ptype):
         """
         me = self.dtype_f(self.init, val=0.0)
 
-        self.fast_acceleration(self.params.npart, self.params.alpha, u.pos.values, me.values[1:self.params.npart - 1])
+        me.values[1:-1] = u.pos.values[:-2] - 2.0 * u.pos.values[1:-1] + u.pos.values[2:] + \
+            self.params.alpha * ((u.pos.values[2:] - u.pos.values[1:-1]) ** 2 -
+                                 (u.pos.values[1:-1] - u.pos.values[:-2]) ** 2)
 
         return me
 
@@ -66,21 +62,12 @@ class fermi_pasta_ulam_tsingou(ptype):
         """
         assert t == 0.0, 'error, u_exact only works for the initial time t0=0'
 
-        me = self.dtype_u(self.init)
+        me = self.dtype_u(self.init, val=0.0)
 
-        for n in range(self.params.npart):
-            me.pos.values[n] = np.sin(self.params.k * np.pi * n / (self.params.npart - 1))
-            me.vel.values[n] = 0.0
+        me.pos.values[:] = np.sin(self.params.k * np.pi * self.xvalues)
+        me.vel.values[:] = 0.0
 
         return me
-
-    @staticmethod
-    @jit
-    def fast_hamiltonian(N, alpha, pos, vel):
-        ham = 0.0
-        for n in range(N - 1):
-            ham += 0.5 * vel[n] ** 2 + 0.5 * (pos[n + 1] - pos[n]) ** 2 + alpha / 3.0 * (pos[n + 1] - pos[n]) ** 3
-        return ham
 
     def eval_hamiltonian(self, u):
         """
@@ -91,7 +78,10 @@ class fermi_pasta_ulam_tsingou(ptype):
         Returns:
             float: hamiltonian
         """
-        return self.fast_hamiltonian(self.params.npart, self.params.alpha, u.pos.values, u.vel.values)
+
+        ham = sum(0.5 * u.vel.values[:-1] ** 2 + 0.5 * (u.pos.values[1:] - u.pos.values[:-1]) ** 2 +
+                  self.params.alpha / 3.0 * (u.pos.values[1:] - u.pos.values[:-1]) ** 3)
+        return ham
 
     def eval_mode_energy(self, u):
         """
@@ -108,12 +98,8 @@ class fermi_pasta_ulam_tsingou(ptype):
 
         for k in self.params.energy_modes:
 
-            Qk = np.sqrt(2.0 / (self.params.npart - 1)) * \
-                sum([u.pos.values[n] * np.sin(np.pi * k * n / (self.params.npart - 1))
-                     for n in range(1, self.params.npart)])
-            Qkdot = np.sqrt(2.0 / (self.params.npart - 1)) * \
-                sum([u.vel.values[n] * np.sin(np.pi * k * n / (self.params.npart - 1))
-                     for n in range(1, self.params.npart)])
+            Qk = np.sqrt(2.0 / (self.params.npart - 1)) * np.dot(u.pos.values, np.sin(np.pi * k * self.xvalues))
+            Qkdot = np.sqrt(2.0 / (self.params.npart - 1)) * np.dot(u.vel.values, np.sin(np.pi * k * self.xvalues))
 
             omegak2 = 4.0 * np.sin(k * np.pi / (2.0 * (self.params.npart - 1))) ** 2
             energy[k] = 0.5 * (Qkdot ** 2 + omegak2 * Qk ** 2)
