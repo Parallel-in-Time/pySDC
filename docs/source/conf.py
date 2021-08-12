fc37ecedeacaf8dd417e595ad4804476426a3930
Robert Speck <r.speck@fz-juelich.de>
2021-08-12 13:25:20 +0200

    working on import issues during sphinx built

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,8 +24,6 @@ sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../../'))
 sys.path.insert(0, os.path.abspath('../../pySDC'))
 
-import pySDC
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
