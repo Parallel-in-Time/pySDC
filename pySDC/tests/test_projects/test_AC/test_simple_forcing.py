7bb9181dbb95120248854bf95463d97aac95f290
Robert Speck <r.speck@fz-juelich.de>
2021-08-12 10:33:27 +0200

    fixed parallel AC test

--- pySDC/tests/test_projects/test_AC/test_simple_forcing.py
+++ pySDC/tests/test_projects/test_AC/test_simple_forcing.py
@@ -1,5 +1,4 @@
 import subprocess
-
 from pySDC.projects.AllenCahn_Bayreuth.run_simple_forcing_verification import main, visualize_radii
 
 def test_main_serial():
@@ -11,19 +10,20 @@ def test_main_parallel():
     # try to import MPI here, will fail if things go wrong (and not later on in the subprocess part)
     import mpi4py
 
-    # my_env = os.environ.copy()
-    # my_env['PYTHONPATH'] = '../../..:.'
     nprocs = 2
-    cmd = f"mpirun -np {nprocs} python pySDC/projects/AllenCahn_Bayreuth/run_simple_forcing_problems.py -n {nprocs}"
+    cmd = f"export PYTHONPATH=$PYTHONPATH:$(pwd); mpirun -np {nprocs} python pySDC/projects/AllenCahn_Bayreuth/run_simple_forcing_benchmark.py -n {nprocs}"
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
     p.wait()
     (output, err) = p.communicate()
     print(output)
+    assert err == ''
 
     nprocs = 4
-    cmd = f"mpirun -np {nprocs} python pySDC/projects/AllenCahn_Bayreuth/run_simple_forcing_problems.py -n {nprocs}"
+    cmd = f"export PYTHONPATH=$PYTHONPATH:$(pwd); mpirun -np {nprocs} python pySDC/projects/AllenCahn_Bayreuth/run_simple_forcing_benchmark.py -n {nprocs}"
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
     p.wait()
     (output, err) = p.communicate()
     print(output)
+    assert err == ''
+
 
