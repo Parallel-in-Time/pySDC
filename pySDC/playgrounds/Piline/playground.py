bfed91313b45611d0373b70709a84f45e9644e09
Robert Speck <r.speck@fz-juelich.de>
2021-12-09 11:45:34 +0100

    piline model works now (SDC)

--- pySDC/playgrounds/Piline/playground.py
+++ pySDC/playgrounds/Piline/playground.py
@@ -5,8 +5,8 @@ from pySDC.helpers.stats_helper import filter_stats, sort_stats
 from pySDC.implementations.collocation_classes.gauss_radau_right import CollGaussRadau_Right
 from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
 from pySDC.implementations.problem_classes.Piline import piline
-# from pySDC.implementations.sweeper_classes.imex_1st_order import imex_1st_order
-from pySDC.implementations.sweeper_classes.generic_LU import generic_LU
+from pySDC.implementations.sweeper_classes.imex_1st_order import imex_1st_order
+# from pySDC.implementations.sweeper_classes.generic_LU import generic_LU
 from pySDC.playgrounds.Piline.log_data import log_data
 import pySDC.helpers.plot_helper as plt_helper
 
@@ -18,12 +18,12 @@ def main():
     # initialize level parameters
     level_params = dict()
     level_params['restol'] = 1E-10
-    level_params['dt'] = 1
+    level_params['dt'] = 0.25
 
     # initialize sweeper parameters
     sweeper_params = dict()
     sweeper_params['collocation_class'] = CollGaussRadau_Right
-    sweeper_params['num_nodes'] = 1
+    sweeper_params['num_nodes'] = 3
     # sweeper_params['QI'] = 'LU'  # For the IMEX sweeper, the LU-trick can be activated for the implicit part
 
     # initialize problem parameters
@@ -38,7 +38,7 @@ def main():
 
     # initialize step parameters
     step_params = dict()
-    step_params['maxiter'] = 1
+    step_params['maxiter'] = 50
 
     # initialize controller parameters
     controller_params = dict()
@@ -49,14 +49,14 @@ def main():
     description = dict()
     description['problem_class'] = piline                  # pass problem class
     description['problem_params'] = problem_params                # pass problem parameters
-    description['sweeper_class'] = generic_LU                 # pass sweeper
+    description['sweeper_class'] = imex_1st_order              # pass sweeper
     description['sweeper_params'] = sweeper_params                # pass sweeper parameters
     description['level_params'] = level_params                    # pass level parameters
     description['step_params'] = step_params                      # pass step parameters
 
     # set time parameters
     t0 = 0.0
-    Tend = 10
+    Tend = 20
 
     # instantiate controller
     controller = controller_nonMPI(num_procs=1, controller_params=controller_params,
