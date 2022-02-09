9ba4a243f3a22a063899974f03aa83e0b831e818
Robert Speck <r.speck@fz-juelich.de>
2022-02-09 18:14:00 +0100

    fixing collocation test accuracy (WTH!)

--- pySDC/tests/test_collocation.py
+++ pySDC/tests/test_collocation.py
@@ -30,7 +30,7 @@ def test_canintegratepolynomials(collclass):
         # use quadrature rule to compute integral
         int_coll = coll.evaluate(coll.weights, poly_vals)
         # For large values of M, substantial differences from different round of error have to be considered
-        assert abs(int_ex - int_coll) < 5e-12, "For node type " + coll.__class__.__name__ + ", failed to integrate polynomial of degree " + str(coll.order-1) + " exactly. Error: %5.3e" % abs(int_ex - int_coll)
+        assert abs(int_ex - int_coll) < 1e-11, "For node type " + coll.__class__.__name__ + ", failed to integrate polynomial of degree " + str(coll.order-1) + " exactly. Error: %5.3e" % abs(int_ex - int_coll)
 
 
 @pytest.mark.parametrize("collclass", classes)
@@ -60,7 +60,7 @@ def test_partialquadraturewithQ(collclass):
         for i in range(0,M):
             int_ex = np.polyval(poly_int_coeff, coll.nodes[i]) - np.polyval(poly_int_coeff, t_start)
             int_coll = np.dot(poly_vals, Q[i,:])
-            assert abs(int_ex - int_coll)< 5e-12, "For node type " + coll.__class__.__name__ + ", partial quadrature from Qmat rule failed to integrate polynomial of degree M-1 exactly for M = " + str(M)
+            assert abs(int_ex - int_coll)< 1e-11, "For node type " + coll.__class__.__name__ + ", partial quadrature from Qmat rule failed to integrate polynomial of degree M-1 exactly for M = " + str(M)
 
 
 @pytest.mark.parametrize("collclass", classes)
