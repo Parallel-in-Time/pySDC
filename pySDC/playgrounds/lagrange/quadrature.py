9e8ffbd682bebc24aacf36d8f5bc083e178ed4ad
Thibaut Lunet <thibaut.lunet@unige.ch>
2022-03-17 20:31:04 +0100

    TL: added original collocation implementation for comparison

--- pySDC/playgrounds/lagrange/quadrature.py
+++ pySDC/playgrounds/lagrange/quadrature.py
@@ -11,6 +11,56 @@ import matplotlib.pyplot as plt
 from pySDC.core import NodesGenerator, LagrangeApproximation
 from pySDC.implementations.collocations import Collocation
 
+from pySDC.core.Errors import CollocationError
+
+from scipy.integrate import quad
+from scipy.interpolate import BarycentricInterpolator
+
+class OriginCollocation(Collocation):
+
+    def _getWeights(self, a, b):
+        """
+        Computes weights using barycentric interpolation
+        Args:
+            a (float): left interval boundary
+            b (float): right interval boundary
+        Returns:
+            numpy.ndarray: weights of the collocation formula given by the nodes
+        """
+        if self.nodes is None:
+            raise CollocationError(
+                "Need nodes before computing weights, got %s" % self.nodes)
+
+        circ_one = np.zeros(self.num_nodes)
+        circ_one[0] = 1.0
+        tcks = []
+        for i in range(self.num_nodes):
+            tcks.append(BarycentricInterpolator(
+                self.nodes, np.roll(circ_one, i)))
+
+        weights = np.zeros(self.num_nodes)
+        for i in range(self.num_nodes):
+            weights[i] = quad(tcks[i], a, b, epsabs=1e-14)[0]
+
+        return weights
+
+    @property
+    def _gen_Qmatrix(self):
+        """
+        Compute tleft-to-node integration matrix for later use in collocation
+        formulation
+        Returns:
+            numpy.ndarray: matrix containing the weights for tleft to node
+        """
+        M = self.num_nodes
+        Q = np.zeros([M + 1, M + 1])
+
+        # for all nodes, get weights for the interval [tleft,node]
+        for m in np.arange(M):
+            Q[m + 1, 1:] = self._getWeights(self.tleft, self.nodes[m])
+
+        return Q
+
 nodeTypes = ['EQUID', 'LEGENDRE']
 quadTypes = ['LOBATTO', 'RADAU-LEFT', 'RADAU-RIGHT', 'GAUSS']
 symbols = ['s', '>', '<', 'o']
@@ -20,9 +70,11 @@ def getLastPlotCol():
 
 nMax = 12
 nVals = np.arange(3, nMax+1)
-tBeg = -1
-tEnd = 1
-nExp = 20
+tBeg = np.random.rand() * 0.2
+tEnd = 0.8 + np.random.rand() * 0.2
+
+
+nExp = 200
 
 
 QMatrixOrder = lambda n: n - 1 - (n % 2)
@@ -78,15 +130,27 @@ def computeQuadratureErrors(nodeType, quadType, numQuad):
 
     for l, n in enumerate(nVals):
 
-        # Generate nodes
-        nodes = nodesGen.getNodes(n)
-
         if numQuad == 'ORIG':
+            # Use origin collocation class
+            coll = OriginCollocation(n, tBeg, tEnd, nodeType, quadType)
+            nodes = coll.nodes
+            weights = coll.weights
+            QMatrix = coll.Qmat[1:, 1:]
+        elif numQuad == 'NEW':
             # Use collocation class
             coll = Collocation(n, tBeg, tEnd, nodeType, quadType)
+            nodes = coll.nodes
             weights = coll.weights
             QMatrix = coll.Qmat[1:, 1:]
         else:
+            # Generate nodes
+            nodes = nodesGen.getNodes(n)
+            a = tBeg
+            b = tEnd
+            nodes += 1
+            nodes /= 2
+            nodes *= b - a
+            nodes += a
             # Set-up Lagrange interpolation polynomial
             approx = LagrangeApproximation(nodes, weightComputation='FAST')
             # Compute quadrature weights for the whole interval
@@ -121,7 +185,7 @@ def plotQuadErrors(nodesType, numQuad, figTitle=False):
         plt.title(title)
         plt.grid(True)
         plt.legend()
-        plt.ylim(1e-17, 1e-12)
+        # plt.ylim(1e-17, 1e-12)
         plt.xlabel('Polynomial degree')
 
     plt.figure()
@@ -140,18 +204,17 @@ def plotQuadErrors(nodesType, numQuad, figTitle=False):
         maxQMatrix = max(maxQMatrix, errs[3].max())
         setFig('QMatrix error')
 
-
     textArgs = dict(
         bbox=dict(boxstyle="round",
                   ec=(0.5, 0.5, 0.5),
                   fc=(0.8, 0.8, 0.8)))
     plt.subplot(1, 2, 1)
     plt.hlines(maxWeigts, 2, nMax, linestyles='--', colors='gray')
-    plt.text(7, 1.5*maxWeigts, f'{maxWeigts:1.2e}', **textArgs)
+    plt.text(7, 0.7*maxWeigts, f'{maxWeigts:1.2e}', **textArgs)
 
     plt.subplot(1, 2, 2)
     plt.hlines(maxQMatrix, 2, nMax, linestyles='--', colors='gray')
-    plt.text(7, 1.5*maxQMatrix, f'{maxQMatrix:1.2e}', **textArgs)
+    plt.text(7, 0.7*maxQMatrix, f'{maxQMatrix:1.2e}', **textArgs)
 
     if figTitle:
         plt.suptitle(f'node distribution : {nodesType}; '
@@ -161,5 +224,5 @@ def plotQuadErrors(nodesType, numQuad, figTitle=False):
 
 if __name__ == '__main__':
     nodesType = 'EQUID'
-    numQuad = 'FEJER'
+    numQuad = 'LEGENDRE_NUMPY'
     plotQuadErrors(nodesType, numQuad)
