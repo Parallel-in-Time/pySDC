c2a4ee0c0cb878bd4bf308f9f8298d8bc3c6dc17
Timo Lenz <lenz3@jsfl02.jusuf>
2022-05-13 17:43:48 +0200

    ac-jusuf-pySDC-gpu.pickle added

--- pySDC/playgrounds/GPU/allen-cahn.py
+++ pySDC/playgrounds/GPU/allen-cahn.py
@@ -1,5 +1,5 @@
-# from pySDC.implementations.problem_classes.AllenCahn_2D_FD import allencahn_semiimplicit
-from pySDC.implementations.problem_classes.AllenCahn_2D_FD_gpu import allencahn_semiimplicit
+from pySDC.implementations.problem_classes.AllenCahn_2D_FD import allencahn_semiimplicit
+# from pySDC.implementations.problem_classes.AllenCahn_2D_FD_gpu import allencahn_semiimplicit
 from pySDC.implementations.collocation_classes.gauss_radau_right import CollGaussRadau_Right
 from pySDC.implementations.sweeper_classes.imex_1st_order import imex_1st_order
 from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
@@ -8,7 +8,7 @@ from pySDC.helpers.stats_helper import filter_stats, sort_stats
 import numpy as np
 import pickle
 import matplotlib.pylab as plt
-name = 'pickle/ac-jusuf-pySDC-gpu.pickle'
+name = 'pickle/ac-jusuf-pySDC-cpu.pickle'
 Ns = np.asarray([128, 256, 512, 1024, 2048])
 # Ns = np.asarray([128, 256, 512])
 times = np.zeros_like(Ns, dtype=float)
