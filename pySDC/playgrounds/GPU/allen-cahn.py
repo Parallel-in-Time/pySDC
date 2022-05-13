97d1fe81b57ea7fd38eda882702a88421ab5daa9
Timo Lenz <t.lenz@fz-juelich.de>
2022-05-13 17:33:46 +0200

    fixed data for pickle, add plots

--- pySDC/playgrounds/GPU/allen-cahn.py
+++ pySDC/playgrounds/GPU/allen-cahn.py
@@ -1,5 +1,5 @@
-from pySDC.implementations.problem_classes.AllenCahn_2D_FD import allencahn_semiimplicit
-# from pySDC.implementations.problem_classes.AllenCahn_2D_FD_gpu import allencahn_semiimplicit
+# from pySDC.implementations.problem_classes.AllenCahn_2D_FD import allencahn_semiimplicit
+from pySDC.implementations.problem_classes.AllenCahn_2D_FD_gpu import allencahn_semiimplicit
 from pySDC.implementations.collocation_classes.gauss_radau_right import CollGaussRadau_Right
 from pySDC.implementations.sweeper_classes.imex_1st_order import imex_1st_order
 from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
@@ -8,20 +8,25 @@ from pySDC.helpers.stats_helper import filter_stats, sort_stats
 import numpy as np
 import pickle
 import matplotlib.pylab as plt
-
+name = 'pickle/ac-jusuf-pySDC-gpu.pickle'
 Ns = np.asarray([128, 256, 512, 1024, 2048])
+# Ns = np.asarray([128, 256, 512])
+times = np.zeros_like(Ns, dtype=float)
+setup = np.zeros_like(Ns, dtype=float)
+cg = np.zeros_like(Ns, dtype=float)
+cg_Count = np.zeros_like(Ns)
+f_im = np.zeros_like(Ns, dtype=float)
+f_ex = np.zeros_like(Ns, dtype=float)
 # initialize problem parameters
 problem_params = dict()
 problem_params['nu'] = 2
 problem_params['eps'] = 0.04
 problem_params['radius'] = 0.25
 # problem_params['nvars'] = [(1024, 1024)]
-# problem_params['newton_maxiter'] = 100
-# problem_params['newton_tol'] = 1E-08
+problem_params['newton_maxiter'] = 100
+problem_params['newton_tol'] = 1E-08
 problem_params['lin_tol'] = 1E-10
 problem_params['lin_maxiter'] = 99
-# problem_params['dtype_u'] = cupy_mesh
-# problem_params['dtype_f'] = cupy_mesh
 
 # initialize level parameters
 level_params = dict()
@@ -43,12 +48,13 @@ step_params['maxiter'] = 50
 
 # setup parameters "in time"
 t0 = 0
-Tend = 8*1E-03
+schritte = 8
+Tend = schritte*1E-03
 
 # initialize controller parameters
 controller_params = dict()
-controller_params['logger_level'] = 10
-for N in Ns:
+controller_params['logger_level'] = 30
+for i, N in enumerate(Ns):
     problem_params['nvars'] = [(N, N)]
     # fill description dictionary for easy step instantiation
     description = dict()
@@ -68,16 +74,33 @@ for N in Ns:
 
     # call main function to get things done...
     uend, stats = controller.run(u0=uinit, t0=t0, Tend=Tend)
+    timing = sort_stats(filter_stats(stats, type='timing_run'), sortby='time')
+    times[i] = timing[0][1]
+    timing_setup = sort_stats(filter_stats(stats, type='timing_setup'), sortby='time')
+    setup[i] = timing_setup[0][1]
+    timing_step = sort_stats(filter_stats(stats, type='timing_step'), sortby='time')
+    timing_step = [ts[1] for ts in timing_step]
+    cg[i] = np.asarray(sum(timing_step))
+    cg_Count[i] = P.lin_ncalls
+    f_im[i] = 0
+    f_ex[i] = 0
+# write down stats to .pickle file
+data = {
+    'Ns': Ns,
+    'D': 2,
+    'dt': level_params['dt'],
+    'schritte': schritte,
+    'iteration': step_params['maxiter'],
+    'Tolerance': problem_params['lin_tol'],
+    'times': times,
+    'setup': setup,
+    'cg-time': cg,
+    'cg-count': cg_Count,
+    'f-time-imp': f_im,
+    'f-time-exp': f_ex
 
-    # write down stats to .pickle file
-    # name = f'pickle/ac-jusuf-pySDC-N{N}-gpu.pickle'
-    name = f'pickle/ac-jusuf-pySDC-N{N}-cpu.pickle'
-    data = {
-        'P': P,
-        'nvars': problem_params['nvars'],
-        'stats': stats
-    }
-    with open(name, 'wb') as f:
-        pickle.dump(data, f)
+}
+with open(name, 'wb') as f:
+    pickle.dump(data, f)
 
 
