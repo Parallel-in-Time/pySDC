112abb9d5c315cf2a179da5dbbc84417db7543bc
Thomas Baumann <t.baumann@fz-juelich.de>
2022-05-20 16:06:44 +0200

    Added a function to the hooks class that counts restarts. This is otherwise difficult because there is no function that does not overwrite entries in the stats object. This function can also be called inside a hook.

--- pySDC/projects/error_estimation/piline_hotrod_adaptivity.py
+++ pySDC/projects/error_estimation/piline_hotrod_adaptivity.py
@@ -22,6 +22,8 @@ class log_data(hooks):
 
         L.sweep.compute_end_point()
 
+        self.count_restarts(step, level_number)
+
         self.add_to_stats(process=step.status.slot, time=L.time, level=L.level_index, iter=0,
                           sweep=L.status.sweep, type='v1', value=L.uend[0])
         self.add_to_stats(process=step.status.slot, time=L.time, level=L.level_index, iter=0,
@@ -112,6 +114,7 @@ def plot(stats, use_adaptivity):
     dt = np.array(sort_stats(filter_stats(stats, type='dt'), sortby='time'))[:, 1]
     e_em = np.array(sort_stats(filter_stats(stats, type='e_embedded'), sortby='time'))[:, 1]
     e_ex = np.array(sort_stats(filter_stats(stats, type='e_extrapolated'), sortby='time'))[:, 1]
+    restarts = np.array(sort_stats(filter_stats(stats, type='restart'), sortby='time'))[:, 1]
     ready = np.logical_and(e_ex != np.array(None), e_em != np.array(None))
 
     assert np.allclose([v1[-1], v2[-2], p3[-1]], [83.88431516810506, 80.62596592922169, 16.134334413301595], rtol=1),\
@@ -122,6 +125,7 @@ def plot(stats, use_adaptivity):
         assert np.isclose(e_ex[-1], 6.565837120935149e-09), 'Extrapolation error when using adaptivity estimate is wro\
             ng!'
         assert np.isclose(dt[-1], 0.0535436291079129), 'Time step size is wrong!'
+        assert np.isclose(restarts.sum(), 1), f'Expected 1 restart, got {restarts.sum()}'
         fig, ax = plt.subplots(1, 1, figsize=(3.5, 3))
         ax.plot(t, v1, label='v1', ls='-')
         ax.plot(t, v2, label='v2', ls='--')
@@ -133,6 +137,7 @@ def plot(stats, use_adaptivity):
         assert np.isclose(e_em[-1], 6.510703087769798e-10), 'Embedded error estimate is wrong!'
         assert np.isclose(e_ex[-1], 6.541069907939809e-10), 'Extrapolation error estimate is wrong!'
         assert np.isclose(dt[-1], 3e-2), 'Time step size is wrong!'
+        assert np.isclose(restarts.sum(), 0), f'Expected 0 restarts, got {restarts.sum()}'
 
     fig, ax = plt.subplots(1, 1, figsize=(3.5, 3))
     ax.plot(t, dt, color='black')
