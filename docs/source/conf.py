c967c98caf11991300bc1e43a2bbf5f22702d44e
Thomas Baumann <t.baumann@fz-juelich.de>
2022-07-20 15:20:56 +0200

    Deleted notebook stuff from the CI pipeline

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -92,7 +91,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['**.ipynb_checkpoints']
+exclude_patterns = []
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
