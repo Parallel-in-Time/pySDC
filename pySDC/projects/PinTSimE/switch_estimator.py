d87a231b293872062c3c49771440de6b1e2bc5c4
lisawim <wimmer@uni-wuppertal.de>
2022-07-22 15:24:58 +0200

    flake8 issues fixed

--- pySDC/projects/PinTSimE/switch_estimator.py
+++ pySDC/projects/PinTSimE/switch_estimator.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy as sp
 
 from pySDC.core.ConvergenceController import ConvergenceController
 from pySDC.core.Lagrange import LagrangeApproximation
@@ -23,7 +22,6 @@ class SwitchEstimator(ConvergenceController):
         if S.status.iter > 0:
             for m in range(len(L.u)):
                 if L.u[m][1] - L.prob.params.V_ref < 0:
-                    m_guess = m - 1
                     self.switch_detected = True
                     break
 
@@ -32,7 +30,7 @@ class SwitchEstimator(ConvergenceController):
 
                 vC_switch = []
                 for m in range(1, len(L.u)):
-                    vC_switch.append(L.u[m][1]-L.prob.params.V_ref)
+                    vC_switch.append(L.u[m][1] - L.prob.params.V_ref)
 
                 approx = LagrangeApproximation(t_interp, weightComputation='AUTO')
 
@@ -43,11 +41,11 @@ class SwitchEstimator(ConvergenceController):
 
                     return approx.getInterpolationMatrix(x).dot(vC_switch)
 
-                vC_interp = switch_examiner(t_interp)    
-                if vC_switch[0]*vC_switch[-1] < 0 and self.dt_adapted == False:
+                vC_interp = switch_examiner(t_interp)
+                if vC_switch[0] * vC_switch[-1] < 0 and not self.dt_adapted:
                     print(vC_interp, t_interp)
                     for m in range(len(t_interp)):
-                        if not np.isclose(vC_interp[m], 0, atol=L.dt*1e-4):
+                        if not np.isclose(vC_interp[m], 0, atol=L.dt * 1e-4):
                             L.status.dt_new = 0.5 * L.dt
 
                         else:
@@ -60,7 +58,6 @@ class SwitchEstimator(ConvergenceController):
                 else:
                     self.switch_detected = False
 
-
     def determine_restart(self, controller, S):
         if self.switch_detected:
             S.status.restart = True
@@ -71,5 +68,5 @@ class SwitchEstimator(ConvergenceController):
     def post_step_processing(self, controller, S):
         self.dt_adapted = False
         S.levels[0].status.dt_new = S.levels[0].params.dt_initial
-        
+
         super(SwitchEstimator, self).determine_restart(controller, S)
