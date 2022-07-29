7a274b075292d5975e17feab9cde0d0685bccaaa
Robert Speck <r.speck@fz-juelich.de>
2022-07-29 18:58:18 +0200

    fixing ModuleNotFoundError

--- pySDC/tests/test_projects/test_AC/test_temp_forcing.py
+++ pySDC/tests/test_projects/test_AC/test_temp_forcing.py
@@ -1,5 +1,4 @@
-from pySDC.projects.AllenCahn_Bayreuth.run_temp_forcing_verification import main
-
-
 def test_main_serial():
+    from pySDC.projects.AllenCahn_Bayreuth.run_temp_forcing_verification import main
+
     main(cwd='pySDC/projects/AllenCahn_Bayreuth/')
