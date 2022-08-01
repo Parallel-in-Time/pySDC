4a2c53aa2d59cd35952eff95dc188b8576c1e916
Robert Speck <r.speck@fz-juelich.de>
2022-08-01 19:25:15 +0200

    debugginf fenics 3

--- pySDC/tests/test_projects/test_TOMS/test_AllenCahn_contracting_circle.py
+++ pySDC/tests/test_projects/test_TOMS/test_AllenCahn_contracting_circle.py
@@ -1,8 +1,8 @@
 import pytest
 
-from pySDC.projects.TOMS.AllenCahn_contracting_circle import main
-
 
 @pytest.mark.slow
 def test_AllenCahn_contracting_circle():
+    from pySDC.projects.TOMS.AllenCahn_contracting_circle import main
+
     main(cwd='pySDC/projects/TOMS/')
