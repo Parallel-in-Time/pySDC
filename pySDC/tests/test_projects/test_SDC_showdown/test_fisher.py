f8eed2fbd20dd020e130a6917d6f4852650a3540
Robert Speck <r.speck@fz-juelich.de>
2022-08-01 19:18:33 +0200

    debugging fenics 2

--- pySDC/tests/test_projects/test_SDC_showdown/test_fisher.py
+++ pySDC/tests/test_projects/test_SDC_showdown/test_fisher.py
@@ -1,8 +1,8 @@
-# import pytest
-#
-#
-# @pytest.mark.petsc
-# def test_fisher():
-#     from pySDC.projects.SDC_showdown.SDC_timing_Fisher import main
-#
-#     main()
+import pytest
+
+
+@pytest.mark.petsc
+def test_fisher():
+    from pySDC.projects.SDC_showdown.SDC_timing_Fisher import main
+
+    main()
