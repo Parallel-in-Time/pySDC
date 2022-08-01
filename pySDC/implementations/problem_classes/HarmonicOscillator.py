3c26ee9e3316e020c6ab4682de6566b5de009de1
Ikrom Akramov <ikrom.akramov@tuhh.de>
2022-08-01 09:56:28 +0200

    I.A: Changes with black

--- pySDC/implementations/problem_classes/HarmonicOscillator.py
+++ pySDC/implementations/problem_classes/HarmonicOscillator.py
@@ -1,7 +1,6 @@
-
 import numpy as np
 
-from pySDC.core.Errors import ParameterError, ProblemError
+from pySDC.core.Errors import ParameterError
 from pySDC.core.Problem import ptype
 from pySDC.implementations.datatype_classes.particles import particles, acceleration
 
@@ -23,23 +22,27 @@ class harmonic_oscillator(ptype):
         """
 
         # these parameters will be used later, so assert their existence
-        problem_params['phase']=1.0
-        problem_params['amp']=0.0
-        essential_keys = ['k', 'mu', 'u0', 'phase', 'amp']
-        
+        problem_params["phase"] = 1.0
+        problem_params["amp"] = 0.0
+        essential_keys = ["k", "mu", "u0", "phase", "amp"]
+
         for key in essential_keys:
-            if key=='mu' and key not in problem_params:
-                problem_params['mu']=0.0
-            if key=='u0' and key not in problem_params:
-                problem_params['u0']=np.array([1, 0])
+            if key == "mu" and key not in problem_params:
+                problem_params["mu"] = 0.0
+            if key == "u0" and key not in problem_params:
+                problem_params["u0"] = np.array([1, 0])
             elif key not in problem_params:
-                msg = 'need %s to instantiate problem, only got %s' % (key, str(problem_params.keys()))
+                msg = "need %s to instantiate problem, only got %s" % (
+                    key,
+                    str(problem_params.keys()),
+                )
                 raise ParameterError(msg)
 
         # invoke super init, passing nparts, dtype_u and dtype_f
-        super(harmonic_oscillator, self).__init__((1, None, np.dtype('float64')), dtype_u, dtype_f, problem_params)
+        super(harmonic_oscillator, self).__init__(
+            (1, None, np.dtype("float64")), dtype_u, dtype_f, problem_params
+        )
 
-        
     def eval_f(self, u, t):
         """
         Routine to compute the RHS
@@ -58,10 +61,10 @@ class harmonic_oscillator(ptype):
 
         u0 = self.params.u0
 
-        u=self.dtype_u(self.init)
+        u = self.dtype_u(self.init)
 
-        u.pos[0]=u0[0]
-        u.vel[0]=u0[1]
+        u.pos[0] = u0[0]
+        u.vel[0] = u0[1]
 
         return u
 
@@ -74,60 +77,55 @@ class harmonic_oscillator(ptype):
         Returns:
             dtype_u: exact position and velocity
         """
-        me=self.dtype_u(self.init)
-        delta=self.params.mu/(2)
-        omega=np.sqrt(self.params.k)
+        me = self.dtype_u(self.init)
+        delta = self.params.mu / (2)
+        omega = np.sqrt(self.params.k)
 
-        U_0=self.params.u0
-        alpha=np.sqrt(np.abs(delta**2-omega**2))
+        U_0 = self.params.u0
+        alpha = np.sqrt(np.abs(delta**2 - omega**2))
         print(self.params.mu)
-        if delta>omega:
+        if delta > omega:
             """
             Overdamped case
             """
 
-            lam_1=-delta+alpha
-            lam_2=-delta-alpha
-            L=np.array([[1,1],[lam_1, lam_2]])
-            A, B=np.linalg.solve(L, U_0)
-            me.pos[:]=A*np.exp(lam_1*t)+B*np.exp(lam_2*t)
-            me.vel[:]=A*lam_1*np.exp(lam_1*t)+B*lam_2*np.exp(lam_2*t)
+            lam_1 = -delta + alpha
+            lam_2 = -delta - alpha
+            L = np.array([[1, 1], [lam_1, lam_2]])
+            A, B = np.linalg.solve(L, U_0)
+            me.pos[:] = A * np.exp(lam_1 * t) + B * np.exp(lam_2 * t)
+            me.vel[:] = A * lam_1 * np.exp(lam_1 * t) + B * lam_2 * np.exp(lam_2 * t)
 
-        elif delta==omega:
+        elif delta == omega:
             """
             Critically damped case
             """
 
-            A=U_0[0]
-            B=U_0[1]+delta*A
-            me.pos[:]=np.exp(-delta*t)*(A+t*B)
-            me.vel[:]=-delta*me.pos[:]+np.exp(-delta*t)*B
+            A = U_0[0]
+            B = U_0[1] + delta * A
+            me.pos[:] = np.exp(-delta * t) * (A + t * B)
+            me.vel[:] = -delta * me.pos[:] + np.exp(-delta * t) * B
 
-        elif delta<omega:
+        elif delta < omega:
             """
             Underdamped case
             """
 
-            lam_1=-delta+alpha*1j
-            lam_2=-delta-alpha*1j
+            lam_1 = -delta + alpha * 1j
+            lam_2 = -delta - alpha * 1j
 
-            M=np.array([[1,1],[lam_1, lam_2]], dtype=complex)
-            A,B=np.linalg.solve(M, U_0)
-           
-            me.pos[:]=np.real(A*np.exp(lam_1*t)+B*np.exp(lam_2*t))
-            me.vel[:]=np.real(A*lam_1*np.exp(lam_1*t)+B*lam_2*np.exp(lam_2*t))
+            M = np.array([[1, 1], [lam_1, lam_2]], dtype=complex)
+            A, B = np.linalg.solve(M, U_0)
+
+            me.pos[:] = np.real(A * np.exp(lam_1 * t) + B * np.exp(lam_2 * t))
+            me.vel[:] = np.real(
+                A * lam_1 * np.exp(lam_1 * t) + B * lam_2 * np.exp(lam_2 * t)
+            )
 
         else:
             pass
             raise ParameterError("Exact solution is not working")
         return me
-
-# =============================================================================
-#         # me = self.dtype_u(self.init)
-#         # me.pos[:] = self.params.amp * np.cos(np.sqrt(self.params.k) * t + self.params.phase)
-#         # me.vel[:] = -self.params.amp * np.sqrt(self.params.k) * np.sin(np.sqrt(self.params.k) * t + self.params.phase)
-#         # return me
-# =============================================================================
 
     def eval_hamiltonian(self, u):
         """
