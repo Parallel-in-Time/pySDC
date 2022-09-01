b60fa6e137d7558ac9b7b73c49da53e3f4778de4
Timo Lenz <t.lenz@fz-juelich.de>
2022-09-01 16:08:51 +0200

    cleaned ac-fft

--- pySDC/playgrounds/GPU/ac-fft.py
+++ pySDC/playgrounds/GPU/ac-fft.py
@@ -5,78 +5,92 @@ from pySDC.implementations.sweeper_classes.imex_1st_order import imex_1st_order
 from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
 from pySDC.helpers.stats_helper import filter_stats, sort_stats
 
-# initialize problem parameters
-problem_params = dict()
-problem_params['nu'] = 2
-problem_params['eps'] = 0.04
-problem_params['radius'] = 0.25
-problem_params['nvars'] = (512, 512)
-problem_params['newton_maxiter'] = 100
-problem_params['newton_tol'] = 1E-08
-problem_params['lin_tol'] = 1E-10
-problem_params['lin_maxiter'] = 99
 
-# initialize level parameters
-level_params = dict()
-level_params['restol'] = 1E-07
-level_params['dt'] = 1E-07
-level_params['nsweeps'] = 1
+def set_parameter():
+    # initialize problem parameters
+    problem_params = dict()
+    problem_params['nu'] = 2
+    problem_params['eps'] = 0.04
+    problem_params['radius'] = 0.25
+    problem_params['nvars'] = (512, 512)
+    problem_params['newton_maxiter'] = 100
+    problem_params['newton_tol'] = 1E-08
+    problem_params['lin_tol'] = 1E-10
+    problem_params['lin_maxiter'] = 99
 
-# initialize sweeper parameters
-sweeper_params = dict()
-sweeper_params['collocation_class'] = CollGaussRadau_Right
-sweeper_params['QI'] = ['LU']
-sweeper_params['QE'] = ['PIC']
-sweeper_params['num_nodes'] = 3
-sweeper_params['initial_guess'] = 'spread'
+    # initialize level parameters
+    level_params = dict()
+    level_params['restol'] = 1E-07
+    level_params['dt'] = 1E-07
+    level_params['nsweeps'] = 1
 
-# initialize step parameters
-step_params = dict()
-step_params['maxiter'] = 50
+    # initialize sweeper parameters
+    sweeper_params = dict()
+    sweeper_params['collocation_class'] = CollGaussRadau_Right
+    sweeper_params['QI'] = ['LU']
+    sweeper_params['QE'] = ['PIC']
+    sweeper_params['num_nodes'] = 3
+    sweeper_params['initial_guess'] = 'spread'
 
-# setup parameters "in time"
-t0 = 0
-schritte = 8
-Tend = schritte*level_params['dt']
+    # initialize step parameters
+    step_params = dict()
+    step_params['maxiter'] = 50
 
-# initialize controller parameters
-controller_params = dict()
-controller_params['logger_level'] = 30
+    # setup parameters "in time"
+    t0 = 0
+    schritte = 8
+    Tend = schritte*level_params['dt']
 
-# fill description dictionary for easy step instantiation cpu
-description = dict()
-description['problem_class'] = ac_fft_cpu
-description['problem_params'] = problem_params  # pass problem parameters
-description['sweeper_class'] = imex_1st_order  # pass sweeper
-description['sweeper_params'] = sweeper_params  # pass sweeper parameters
-description['level_params'] = level_params  # pass level parameters
-description['step_params'] = step_params  # pass step parameters
+    # initialize controller parameters
+    controller_params = dict()
+    controller_params['logger_level'] = 30
 
-# instantiate controller cpu
-controller = controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
+    # fill description dictionary for easy step instantiation
+    description = dict()
+    description['problem_params'] = problem_params  # pass problem parameters
+    description['sweeper_class'] = imex_1st_order  # pass sweeper
+    description['sweeper_params'] = sweeper_params  # pass sweeper parameters
+    description['level_params'] = level_params  # pass level parameters
+    description['step_params'] = step_params  # pass step parameters
 
-# get initial values on finest level cpu
-P = controller.MS[0].levels[0].prob
-uinit = P.u_exact(t0)
+    return controller_params, description, t0, Tend
 
-# call main function to get things done on cpu...
-uend_cpu, stats_cpu = controller.run(u0=uinit, t0=t0, Tend=Tend)
-timing_cpu = sort_stats(filter_stats(stats_cpu, type='timing_run'), sortby='time')
-print('Laufzeit:', timing_cpu[0][1])
 
-# change description dictionary for easy step instantiation for gpu
-description['problem_class'] = ac_fft_gpu
+def main():
+    controller_params, description, t0, Tend = set_parameter()
 
-# instantiate controller cpu
-controller = controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
+    # fill description dictionary with CPU problem
+    description['problem_class'] = ac_fft_cpu
 
-# get initial values on finest level cpu
-P = controller.MS[0].levels[0].prob
-uinit = P.u_exact(t0)
+    # instantiate controller cpu
+    controller = controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
 
-# call main function to get things done on cpu...
-uend_gpu, stats_gpu = controller.run(u0=uinit, t0=t0, Tend=Tend)
-timing_gpu = sort_stats(filter_stats(stats_gpu, type='timing_run'), sortby='time')
-print('Laufzeit:', timing_gpu[0][1])
+    # get initial values on finest level cpu
+    P = controller.MS[0].levels[0].prob
+    uinit = P.u_exact(t0)
 
-assert abs(uend_gpu-uend_cpu) < 10E-15
+    # call main function to get things done on cpu...
+    uend_cpu, stats_cpu = controller.run(u0=uinit, t0=t0, Tend=Tend)
+    timing_cpu = sort_stats(filter_stats(stats_cpu, type='timing_run'), sortby='time')
+    print('Runtime CPU:', timing_cpu[0][1])
+
+    # change description dictionary with GPU problem
+    description['problem_class'] = ac_fft_gpu
+
+    # instantiate controller cpu
+    controller = controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
+
+    # get initial values on finest level cpu
+    P = controller.MS[0].levels[0].prob
+    uinit = P.u_exact(t0)
+
+    # call main function to get things done on cpu...
+    uend_gpu, stats_gpu = controller.run(u0=uinit, t0=t0, Tend=Tend)
+    timing_gpu = sort_stats(filter_stats(stats_gpu, type='timing_run'), sortby='time')
+    print('Runtime GPU:', timing_gpu[0][1])
+
+    assert abs(uend_gpu-uend_cpu) < 10E-15
+
+
+if __name__ == '__main__':
+    main()
