b1df96f4983455db10b8b41e1fc9ce67aa9462ba
Thomas Baumann <t.baumann@fz-juelich.de>
2022-09-14 00:04:14 +0200

    Added unit test for the Runge-Kutta sweeper

--- pySDC/playgrounds/Runge_Kutta/test_order.py
+++ pySDC/playgrounds/Runge_Kutta/test_order.py
@@ -20,6 +20,18 @@ def plot_order(sweeper, prob, dt_list, description=None, ax=None, Tend_fixed=Non
     # determine the order
     plot_orders(ax, [1], True, Tend_fixed=Tend_fixed, custom_description=description, dt_list=dt_list, prob=prob)
 
+    # check if we got the expected order for the local error
+    orders = {
+        RK1: 2,
+        MidpointMethod: 3,
+        RK4: 5,
+        CrankNicholson: 3,
+    }
+    numerical_order = float(ax.get_lines()[-1].get_label()[7:])
+    expected_order = orders.get(sweeper, numerical_order)
+    assert np.isclose(numerical_order, expected_order, atol=2.5e-1),\
+        f"Expected order {expected_order}, got {numerical_order}!"
+
     # decorate
     colors = {
         RK1: 'blue',
@@ -34,24 +46,21 @@ def plot_order(sweeper, prob, dt_list, description=None, ax=None, Tend_fixed=Non
     ax.legend(frameon=False)
 
 
-def plot_all_orders(prob, dt_list, Tend):
+def plot_all_orders(prob, dt_list, Tend, sweepers):
     fig, ax = plt.subplots(1, 1)
-    sweepers = [RK1]
-    sweepers = [RK4]
-    sweepers = [MidpointMethod]
-    sweepers = [RK1, MidpointMethod, CrankNicholson, RK4]
     for i in range(len(sweepers)):
         plot_order(sweepers[i], prob, dt_list, Tend_fixed=Tend, ax=ax)
 
 
 def test_vdp():
     Tend = 5e-2
-    plot_all_orders(run_vdp, Tend * 2.**(-np.arange(8)), Tend)
+    plot_all_orders(run_vdp, Tend * 2.**(-np.arange(8)), Tend, [RK1, MidpointMethod, CrankNicholson, RK4])
 
 
 def test_advection():
-    plot_all_orders(run_advection, 1.e-3 * 2.**(-np.arange(8)), None)
+    plot_all_orders(run_advection, 1.e-3 * 2.**(-np.arange(8)), None, [RK1, MidpointMethod, CrankNicholson])
 
 
 if __name__ == '__main__':
     test_vdp()
+    test_advection()
