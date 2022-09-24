ea54d5e7e4cba38412224d564b2c825ebba072ea
Robert Speck <r.speck@fz-juelich.de>
2022-09-24 09:59:05 +0200

    CuPy test should work NOW

--- pySDC/projects/GPU/ac_fft.py
+++ pySDC/projects/GPU/ac_fft.py
@@ -91,7 +91,7 @@ def main():
     timing_gpu = sort_stats(filter_stats(stats_gpu, type='timing_run'), sortby='time')
     print('Runtime GPU:', timing_gpu[0][1])
 
-    assert abs(uend_gpu.get() - uend_cpu) < 1e-14, abs(uend_gpu.get() - uend_cpu)
+    assert abs(uend_gpu.get() - uend_cpu) < 1e-13, abs(uend_gpu.get() - uend_cpu)
 
 
 if __name__ == '__main__':
