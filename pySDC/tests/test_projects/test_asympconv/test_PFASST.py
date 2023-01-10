1f6470cb2f8af620cfeb6cc9688bc4766af7eb75
Robert Speck <pancetta@users.noreply.github.com>
2023-01-10 14:10:23 +0100

    Better tests of asympconf_Linf

--- pySDC/tests/test_projects/test_asympconv/test_PFASST.py
+++ pySDC/tests/test_projects/test_asympconv/test_PFASST.py
@@ -7,6 +7,18 @@ def test_main():
 
     main()
 
+@pytest.mark.base
+def test_Linf():
+    from pySDC.projects.AsympConv.PFASST_conv_Linf import run_advection, run_diffusion
+
+    QI = 'LU'
+    run_diffusion(QI=QI, max_proc_exp=4)
+    run_advection(QI=QI, max_proc_exp=4)
+
+    QI = 'LU2'
+    run_diffusion(QI=QI, max_proc_exp=4)
+    run_advection(QI=QI, max_proc_exp=4)
+
 
 @pytest.mark.base
 def test_plot_results():
