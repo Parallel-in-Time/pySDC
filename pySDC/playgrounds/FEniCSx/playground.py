d77be3c92a663b29945ab3d9b6aab2df64e39c49
Robert Speck <pancetta@users.noreply.github.com>
2023-01-10 11:33:45 +0100

    Update playground.py

--- pySDC/playgrounds/FEniCSx/playground.py
+++ pySDC/playgrounds/FEniCSx/playground.py
@@ -3,79 +3,6 @@ from mpi4py import MPI
 import numpy as np
 
 
-class fenics_mesh(fem.Function):
-    """
-    Numpy-based datatype for serial or parallel meshes.
-    Can include a communicator and expects a dtype to allow complex data.
-
-    Attributes:
-        _comm: MPI communicator or None
-    """
-
-    def __new__(cls, init, val=0.0):
-        """
-        Instantiates new datatype. This ensures that even when manipulating data, the result is still a mesh.
-
-        Args:
-            init: either another mesh or a tuple containing the dimensions, the communicator and the dtype
-            val: value to initialize
-
-        Returns:
-            obj of type mesh
-
-        """
-        if isinstance(init, fenics_mesh):
-            obj = init.copy()
-        elif (
-            isinstance(init, fem.FunctionSpace)
-            # isinstance(init, tuple)
-            # and (init[1] is None or isinstance(init[1], MPI.Intracomm))
-            # and isinstance(init[2], np.dtype)
-        ):
-            obj = fem.Function.__new__(cls)
-            print(obj)
-            # obj = fem.Function(init)
-            obj.x.array[:] = val
-        elif (
-            isinstance(init, fem.function.Function)
-        ):
-            obj = init.copy()
-        else:
-            raise NotImplementedError(type(init))
-        return obj
-
-    def interpolate(self, u, cells=None):
-        tmp = fem.Function(self.function_space)
-        tmp.interpolate(u)
-        return fenics_mesh(tmp)
-
-    def isend(self, dest=None, tag=None, comm=None):
-        """
-        Routine for sending data forward in time (non-blocking)
-
-        Args:
-            dest (int): target rank
-            tag (int): communication tag
-            comm: communicator
-
-        Returns:
-            request handle
-        """
-        return comm.Issend(self.x.array[:], dest=dest, tag=tag)
-
-    def irecv(self, source=None, tag=None, comm=None):
-        """
-        Routine for receiving in time
-
-        Args:
-            source (int): source rank
-            tag (int): communication tag
-            comm: communicator
-
-        Returns:
-            None
-        """
-        return comm.Irecv(self.x.array[:], source=source, tag=tag)
 
 def initial_condition(x, a=5):
     return np.exp(-a*(x[0]**2+x[1]**2))
@@ -88,26 +15,29 @@ print(comm_fenics.Get_size())
 nx, ny = 50, 50
 domain = mesh.create_rectangle(comm_fenics, [np.array([-2, -2]), np.array([2, 2])],
                                [nx, ny], mesh.CellType.triangle)
-V = fem.FunctionSpace(domain, ("CG", 1))
+V = fem.FunctionSpace(domain, ("CG", 4))
+nxc, nyc = 50, 50
+domainc = mesh.create_rectangle(comm_fenics, [np.array([-2, -2]), np.array([2, 2])],
+                               [nxc, nyc], mesh.CellType.triangle)
+Vc = fem.FunctionSpace(domain, ("CG", 1))  ## WORKS
+Vc = fem.FunctionSpace(domainc, ("CG", 1))  ## DOES NOT WORK
 
-a = fenics_mesh(V)
-# a.interpolate(initial_condition)
-print(type(a))
+u = fem.Function(V)
+uc = fem.Function(Vc)
 
-b = fenics_mesh(a)
-print(type(b))
+u.interpolate(lambda x: initial_condition(x,a=5))
 
-d = a + b
-print(type(d))
+uc.interpolate(u)
 
-w_n = fem.Function(V)
-w_n.name = "w_n"
-w_n.x.array[:] = 99
 
-print(np.mean(w_n.x.array))
-
-if comm.Get_size() == 2:
-    if comm.Get_rank() == 0:
-        comm.send(u_n.x.array, dest=1)
-    else:
-        v_n.x.array[:] = comm.recv(source=0)
+# w_n = fem.Function(V)
+# w_n.name = "w_n"
+# w_n.x.array[:] = 99
+#
+# print(np.mean(w_n.x.array))
+#
+# if comm.Get_size() == 2:
+#     if comm.Get_rank() == 0:
+#         comm.send(u_n.x.array, dest=1)
+#     else:
+#         v_n.x.array[:] = comm.recv(source=0)
