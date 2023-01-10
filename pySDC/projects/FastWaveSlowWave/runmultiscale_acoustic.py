1030f7e109166f2d8859cc4c3fdda71bda23eea6
Robert Speck <pancetta@users.noreply.github.com>
2023-01-10 14:03:16 +0100

    better testing of (outdated) standard_integrators

--- pySDC/projects/FastWaveSlowWave/runmultiscale_acoustic.py
+++ pySDC/projects/FastWaveSlowWave/runmultiscale_acoustic.py
@@ -126,6 +126,10 @@ def compute_and_plot_solutions():
     x_0 = 0.75
     # x_1 = 0.25
 
+    assert np.isclose(np.linalg.norm(uend[1, :], np.inf), 8.489e-01, 1E-03)
+    assert np.isclose(np.linalg.norm(pnew_dirk, np.inf), 1.003e+00, 1E-03)
+    assert np.isclose(np.linalg.norm(pnew_imex, np.inf), 2.762e+21, 1E-03)
+
     print('Maximum pressure in SDC: %5.3e' % np.linalg.norm(uend[1, :], np.inf))
     print('Maximum pressure in DIRK: %5.3e' % np.linalg.norm(pnew_dirk, np.inf))
     print('Maximum pressure in RK-IMEX: %5.3e' % np.linalg.norm(pnew_imex, np.inf))
