7c33bd0fd42e45d4aaed397d379d9d973658eae9
Thomas Baumann <t.baumann@fz-juelich.de>
2023-01-12 16:22:26 +0100

    Forgot to add some files to the commit

---
+++ pySDC/implementations/hooks/log_embedded_error_estimate.py
@@ -0,0 +1,31 @@
+from pySDC.core.Hooks import hooks
+
+
+class LogEmbeddedErrorEstimate(hooks):
+    """
+    Store the embedded error estimate at the end of each step as "error_embedded_estimate".
+    """
+
+    def post_step(self, step, level_number):
+        """
+        Record embedded error estimate
+
+        Args:
+            step (pySDC.Step.step): the current step
+            level_number (int): the current level number
+
+        Returns:
+            None
+        """
+        # some abbreviations
+        L = step.levels[level_number]
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type='error_embedded_estimate',
+            value=L.status.get('error_embedded_estimate'),
+        )
---
+++ pySDC/implementations/hooks/log_solution.py
@@ -0,0 +1,32 @@
+from pySDC.core.Hooks import hooks
+
+
+class LogSolution(hooks):
+    """
+    Store the solution at the end of each step as "u".
+    """
+
+    def post_step(self, step, level_number):
+        """
+        Record solution at the end of the step
+
+        Args:
+            step (pySDC.Step.step): the current step
+            level_number (int): the current level number
+
+        Returns:
+            None
+        """
+        # some abbreviations
+        L = step.levels[level_number]
+        L.sweep.compute_end_point()
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type='u',
+            value=L.uend,
+        )
