76b5045b7b7ba231da6b691ba9cfeff61aa5ea69
Thomas Baumann <t.baumann@fz-juelich.de>
2023-01-20 08:38:14 +0100

    Renamed file, thanks for noticing @lisawim

--- pySDC/tests/test_projects/test_resilience/test_Lorenz.py
+++ pySDC/tests/test_projects/test_resilience/test_Lorenz.py
@@ -3,6 +3,6 @@ import pytest
 
 @pytest.mark.base
 def test_main():
-    from pySDC.projects.Resilience.Lorentz import main
+    from pySDC.projects.Resilience.Lorenz import main
 
     main(plotting=False)
