fc0d807caf4b40a18d8c1a10b04dba2caa69706a
Thomas Baumann <t.baumann@fz-juelich.de>
2023-01-23 12:09:04 +0100

    Added collocation inexactness convergence controller

---
+++ pySDC/implementations/convergence_controller_classes/collocation_inexactness.py
@@ -0,0 +1,162 @@
+import numpy as np
+
+from pySDC.core.Lagrange import LagrangeApproximation
+from pySDC.core.ConvergenceController import ConvergenceController, Status
+from pySDC.core.Collocation import CollBase
+
+
+class CollocationInexactness(ConvergenceController):
+    def setup(self, controller, params, description):
+        """
+        Record what variables we want to vary.
+
+        Args:
+            controller (pySDC.Controller.controller): The controller
+            params (dict): The params passed for this specific convergence controller
+            description (dict): The description object used to instantiate the controller
+
+        Returns:
+            (dict): The updated params dictionary
+        """
+
+        defaults = {
+            'control_order': 300,
+            'num_colls': 0,
+            'sweeper_params': description['sweeper_params'],
+            'vary_keys_sweeper': [],
+            'vary_keys_level': [],
+        }
+
+        # only these keys can be changed by this convergence controller
+        self.allowed_sweeper_keys = ['quad_type', 'num_nodes', 'node_type']
+        self.allowed_level_keys = ['restol']
+
+        # add the keys to lists so we know what we need to change later
+        for key in params.keys():
+            if type(params[key]) == list:
+                if key in self.allowed_sweeper_keys:
+                    defaults['vary_keys_sweeper'] += [key]
+                elif key in self.allowed_level_keys:
+                    defaults['vary_keys_level'] += [key]
+                else:
+                    raise NotImplementedError(f'Don\'t know what to do with key {key} here!')
+
+                defaults['num_colls'] = max([defaults['num_colls'], len(params[key])])
+
+        return {**defaults, **params}
+
+    def switch_sweeper(self, S):
+        """
+        Update to the next sweeper in line.
+
+        Args:
+            S (pySDC.Step.step): The current step
+
+        Returns:
+            None
+        """
+
+        # generate dictionaries with the new parameters
+        new_params_sweeper = {
+            key: self.params.get(key)[self.status.active_coll] for key in self.params.vary_keys_sweeper
+        }
+        sweeper_params = self.params.sweeper_params.copy()
+        update_params_sweeper = {**sweeper_params, **new_params_sweeper}
+
+        new_params_level = {key: self.params.get(key)[self.status.active_coll] for key in self.params.vary_keys_level}
+
+        # update sweeper for all levels
+        for L in S.levels:
+            P = L.prob
+
+            # store solution of current level which will be interpolated to new level
+            u_old = L.u.copy()
+            nodes_old = L.sweep.coll.nodes.copy()
+
+            # change sweeper
+            L.sweep.__init__(update_params_sweeper)
+            L.sweep.level = L
+
+            # reset level to tell it the new structure of the solution
+            L.params.__dict__.update(new_params_level)
+            L.reset_level(reset_status=False)
+
+            # interpolate solution of old collocation problem to new one
+            nodes_new = L.sweep.coll.nodes.copy()
+            interpolator = LagrangeApproximation(points=np.append(0, nodes_old))
+            u_inter = interpolator.getInterpolationMatrix(np.append(0, nodes_new)) @ u_old
+            assert 0 not in nodes_new or 0 not in nodes_old, "Don't know what to do when left is node yet"
+
+            # assign the interpolated values to the nodes in the level
+            for i in range(0, len(u_inter)):
+                me = P.dtype_u(P.init)
+                me[:] = u_inter[i]
+                L.u[i] = me
+
+            # reevaluate rhs
+            for i in range(L.sweep.coll.num_nodes + 1):
+                L.f[i] = L.prob.eval_f(L.u[i], L.time)
+
+        # log the new parameters
+        msg = f'Switching to collocation {self.status.active_coll + 1} of {self.params.num_colls}:'
+        for key in list(sweeper_params.keys()) + list(new_params_level.keys()):
+            if key in self.params.vary_keys_sweeper:
+                msg += f'\n--> {key}: {update_params_sweeper[key]}'
+            elif key in self.params.vary_keys_level:
+                msg += f'\n--> {key}: {new_params_level[key]}'
+            else:
+                msg += f'\n    {key}: {update_params_sweeper[key]}'
+        self.log(msg, S)
+
+    def setup_status_variables(self, controller, **kwargs):
+        """
+        Add an index for which collocation method to use.
+
+        Args:
+            controller (pySDC.Controller.controller): The controller
+
+        Returns:
+            None
+        """
+        self.status = Status(['active_coll'])
+
+    def reset_status_variables(self, controller, **kwargs):
+        """
+        Reset the status variables between time steps.
+
+        Args:
+            controller (pySDC.Controller.controller): The controller
+
+        Returns:
+            None
+        """
+        self.status.active_coll = 0
+
+    def post_iteration_processing(self, controller, S, **kwargs):
+        """
+        Switch to the next collocation method if the current one is converged.
+
+        Args:
+            controller (pySDC.Controller.controller): The controller
+            S (pySDC.Step.step): The current step
+
+        Returns:
+            None
+        """
+        if (self.status.active_coll < self.params.num_colls - 1) and S.status.done:
+            self.status.active_coll += 1
+            S.status.done = False
+            self.switch_sweeper(S)
+
+    def post_spread_processing(self, controller, S, **kwargs):
+        """
+        Overwrite the sweeper parameters with the first collocation parameters set up here.
+
+        Args:
+            controller (pySDC.Controller.controller): The controller
+            S (pySDC.Step.step): The current step
+
+        Returns:
+            None
+        """
+        self.switch_sweeper(S)
---
+++ pySDC/projects/Resilience/collocation_adaptivity.py
@@ -0,0 +1,179 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import TABLEAU_COLORS
+
+from pySDC.helpers.stats_helper import get_sorted
+from pySDC.projects.Resilience.vdp import run_vdp
+from pySDC.projects.Resilience.advection import run_advection
+from pySDC.projects.Resilience.heat import run_heat
+from pySDC.projects.Resilience.hook import LogData
+from pySDC.implementations.convergence_controller_classes.collocation_inexactness import CollocationInexactness
+from pySDC.core.Hooks import hooks
+
+
+CMAP = list(TABLEAU_COLORS.values())
+
+
+class LogSweeperParams(hooks):
+    """
+    Log the sweeper parameters after every iteration to check if the collocation inexactness is doing what it's supposed
+    to.
+    """
+
+    def post_iteration(self, step, level_number):
+        """
+        Args:
+            step (pySDC.Step.step): the current step
+            level_number (int): the current level number
+
+        Returns:
+            None
+        """
+        super().post_iteration(step, level_number)
+
+        L = step.levels[level_number]
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type='sweeper_params',
+            value=L.sweep.params.__dict__,
+        )
+
+
+class LogLocalErrorPostIter(hooks):
+    """
+    Log the local error after each iteration
+    """
+
+    def post_iteration(self, step, level_number):
+        """
+        Args:
+            step (pySDC.Step.step): the current step
+            level_number (int): the current level number
+
+        Returns:
+            None
+        """
+        super().post_iteration(step, level_number)
+
+        L = step.levels[level_number]
+        L.sweep.compute_end_point()
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type='e_local_post_iter',
+            value=abs(L.prob.u_exact(t=L.time + L.dt) - L.uend),
+        )
+
+
+def compare_collocation_inexactness(prob):
+    """
+    Run a problem with various modes of collocation inexactness.
+
+    Args:
+        prob (function): A problem from the resilience project to run
+
+    Returns:
+        None
+    """
+    fig, ax = plt.subplots()
+    node_ax = ax.twinx()
+
+    Tend = 0.015
+    base_params = {
+        'step_params': {'maxiter': 99},
+        'sweeper_params': {
+            'QI': 'LU',
+            'num_nodes': 4,
+        },
+        'level_params': {'restol': 1e-9, 'dt': Tend},
+    }
+
+    coll_params_inexact = {
+        'num_nodes': [2, 3, 4],
+        'restol': [1e-4, 1e-7, 1e-9],
+    }
+    coll_params_refinement = {
+        'num_nodes': [3, 4],
+    }
+    coll_params_reduce = {
+        'num_nodes': [4, 3],
+    }
+
+    special_params = {
+        'inexact': {CollocationInexactness: coll_params_inexact},
+        'refinement': {CollocationInexactness: coll_params_refinement},
+        'reduce': {CollocationInexactness: coll_params_reduce},
+        'standard': {},
+    }
+
+    for i in range(len(special_params.keys())):
+        key = list(special_params.keys())[i]
+        custom_description = {**base_params, 'convergence_controllers': special_params[key]}
+        custom_controller_parameters = {'logger_level': 30}
+        stats, _, _ = prob(
+            Tend=Tend,
+            custom_description=custom_description,
+            custom_controller_params=custom_controller_parameters,
+            hook_class=[LogData, LogSweeperParams, LogLocalErrorPostIter],
+        )
+
+        plot_residual(stats, ax, node_ax, label=key, color=CMAP[i])
+
+
+def plot_residual(stats, ax, node_ax, **kwargs):
+    """
+    Plot residual and nodes vs. iteration.
+    Also a test is performed to see if we can reproduce previously obtained results.
+
+    Args:
+        stats (pySDC.stats): The stats object of the run
+        ax (Matplotlib.pyplot.axes): Somewhere to plot
+        node_ax (Matplotlib.pyplot.axes): Somewhere to plot
+
+    Returns:
+        None
+    """
+    sweeper_params = get_sorted(stats, type='sweeper_params', sortby='iter')
+    residual = get_sorted(stats, type='residual_post_iteration', sortby='iter')
+
+    # determine when the number of collocation nodes increased
+    nodes = [me[1]['num_nodes'] for me in sweeper_params]
+
+    # test if the expected outcome was achieved
+    label = kwargs['label']
+    expect = {
+        'inexact': [2, 2, 3, 3, 4, 4],
+        'refinement': [3, 3, 3, 3, 3, 3, 4, 4],
+        'reduce': [4, 4, 4, 4, 4, 4, 3, 3],
+        'standard': [4, 4, 4, 4, 4, 4],
+    }
+    assert np.allclose(
+        nodes, expect[label]
+    ), f"Unexpected distribution of nodes vs. iteration in {label}! Expected {expect[label]}, got {nodes}"
+
+    ax.plot([me[0] for me in residual], [me[1] for me in residual], **kwargs)
+    ax.set_yscale('log')
+    ax.legend(frameon=False)
+    ax.set_xlabel(r'$k$')
+    ax.set_ylabel(r'residual')
+
+    node_ax.plot([me[0] for me in sweeper_params], nodes, **kwargs, ls='--')
+    node_ax.set_ylabel(r'nodes')
+
+
+def main():
+    compare_collocation_inexactness(run_vdp)
+
+
+if __name__ == "__main__":
+    main()
+    plt.show()
