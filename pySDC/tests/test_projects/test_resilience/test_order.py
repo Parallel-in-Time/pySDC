f803dbe63408052f8f01498dcef04b12b3bd9859
Thomas Baumann <t.baumann@fz-juelich.de>
2023-04-28 16:08:58 +0200

    Made tests of order in time in resilience project shorter

--- pySDC/tests/test_projects/test_resilience/test_order.py
+++ pySDC/tests/test_projects/test_resilience/test_order.py
@@ -2,7 +2,21 @@ import pytest
 
 
 @pytest.mark.base
-def test_main():
-    from pySDC.projects.Resilience.accuracy_check import main
+@pytest.mark.parametrize("ks", [[2], [3], [4]])
+@pytest.mark.parametrize("serial", [True, False])
+def test_order_fixed_step_size(ks, serial):
+    from pySDC.projects.Resilience.accuracy_check import plot_all_errors, plt
 
-    main()
+    fig, ax = plt.subplots()
+    plot_all_errors(ax, ks, serial, Tend_fixed=1.0)
+
+
+@pytest.mark.base
+@pytest.mark.parametrize("ks", [[2], [3]])
+@pytest.mark.parametrize("serial", [True, False])
+def test_order_adaptive_step_size(ks, serial):
+    print(locals())
+    from pySDC.projects.Resilience.accuracy_check import plot_all_errors, plt
+
+    fig, ax = plt.subplots()
+    plot_all_errors(ax, ks, serial, Tend_fixed=5e-1, var='e_tol', dt_list=[1e-5, 5e-6], avoid_restarts=False)
