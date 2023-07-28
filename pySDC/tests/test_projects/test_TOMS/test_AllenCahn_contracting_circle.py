941181702c986d89b9e8d738cb1db7f856b05968
Robert Speck <pancetta@users.noreply.github.com>
2023-07-28 16:18:53 +0200

    Yes, black, yes..

--- pySDC/tests/test_projects/test_TOMS/test_AllenCahn_contracting_circle.py
+++ pySDC/tests/test_projects/test_TOMS/test_AllenCahn_contracting_circle.py
@@ -4,14 +4,19 @@ import os
 
 results = {}
 
+
 @pytest.mark.base
 @pytest.mark.slow
 @pytest.mark.order(1)
-@pytest.mark.parametrize("variant", ['multi-implicit', 'semi-implicit', 'fully-implicit', 'semi-implicit_v2', 'multi-implicit_v2'])
+@pytest.mark.parametrize(
+    "variant", ['multi-implicit', 'semi-implicit', 'fully-implicit', 'semi-implicit_v2', 'multi-implicit_v2']
+)
 @pytest.mark.parametrize("inexact", [False, True])
 def test_AllenCahn_contracting_circle(variant, inexact):
     from pySDC.projects.TOMS.AllenCahn_contracting_circle import run_SDC_variant
+
     results[(variant, 'exact' if not inexact else 'inexact')] = run_SDC_variant(variant=variant, inexact=inexact)
+
 
 @pytest.mark.base
 @pytest.mark.order(2)
@@ -28,4 +33,3 @@ def test_show_results():
 
     # visualize
     show_results(fname, cwd=cwd)
-
