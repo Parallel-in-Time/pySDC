d547fd3b55d412880c79137b45ce890dff1377c4
Thibaut Lunet <thibaut.lunet@tuhh.de>
2023-11-13 11:49:01 +0700

    TL: first vanderpol plots error VS time-step size

--- pySDC/projects/parallelSDC/Paper/vanderpol_accuracy.py
+++ pySDC/projects/parallelSDC/Paper/vanderpol_accuracy.py
@@ -12,34 +12,57 @@ in particular with graphs such as :
 Note : implementation in progress ...
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 from utils import getParamsSDC, solVanderpolSDC, solVanderpolExact
 
-muVals = [0.1, 1, 10]
-tEndVals = [6.3, 6.7, 18.9] # 1 periods for each mu
+muVals = [0.1, 2, 10]
+tEndVals = [6.3, 7.6, 18.9] # 1 periods for each mu
 
 def getError(uNum, uRef):
     if uNum is None:
         return np.inf
     return np.linalg.norm(uRef[:, 0] - uNum[:, 0], np.inf)
 
-
+# Base variable parameters
 qDeltaList = ['LU', 'IEpar', 'MIN-SR-NS', 'MIN-SR-S', 'FLEX-MIN-1', 'MIN3']
+nStepsList = np.array([2, 5, 10, 100, 200, 500, 1000])
+nSweepList = [1, 2, 3, 4]
 
+
+nSweepList = [3]
 
 for mu, tEnd in zip(muVals, tEndVals):
     print("-"*80)
     print(f"mu={mu}")
     print("-"*80)
 
-    for nSteps in [1, 10, 100, 200, 500]:
-        print(f"nSteps={nSteps}")
+    dtVals = tEnd/nStepsList
 
-        uRef = solVanderpolExact(tEnd, nSteps, mu=mu)
+    plt.figure(f'mu={mu}')
+    for qDelta in qDeltaList:
+        for nSweeps in nSweepList:
+            name = f"{qDelta}({nSweeps})"
+            print(f'computing for {name} ...')
 
-        for qDeltaI in qDeltaList:
-            paramsSDC = getParamsSDC(qDeltaI=qDeltaI)
-            uSDC_LU = solVanderpolSDC(tEnd, nSteps, paramsSDC, mu=mu)
+            errors = []
 
-            err = getError(uSDC_LU, uRef)
-            print(f"  {qDeltaI} -- err={err:1.2g}")
+            for nSteps in nStepsList:
+                print(f' -- nSteps={nSteps} ...')
+
+                uRef = solVanderpolExact(tEnd, nSteps, mu=mu)
+
+                paramsSDC = getParamsSDC(qDeltaI=qDelta, nSweeps=nSweeps)
+                uSDC = solVanderpolSDC(tEnd, nSteps, paramsSDC, mu=mu)
+
+                err = getError(uSDC, uRef)
+                # some hack for very stiff configurations
+                if mu == 10 and nSteps == 2:
+                    err = np.inf
+                errors.append(err)
+
+            plt.loglog(dtVals, errors, 'o-', label=name)
+    plt.legend()
+    plt.xlabel(r"$\Delta{t}$")
+    plt.ylabel(r"$L_\infty$ error")
+    plt.grid()
