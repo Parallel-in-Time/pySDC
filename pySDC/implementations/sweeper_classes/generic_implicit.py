a9f05c0b01fabed1fe9191093477c19323074ac1
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2023-11-20 13:20:10 +0100

    Refactor of `update_nodes` in generic implicit sweeper

--- pySDC/implementations/sweeper_classes/generic_implicit.py
+++ pySDC/implementations/sweeper_classes/generic_implicit.py
@@ -20,33 +20,9 @@ class generic_implicit(sweeper):
         if 'QI' not in params:
             params['QI'] = 'IE'
 
-        # call parent's initialization routine
         super().__init__(params)
 
-        # get QI matrix
         self.QI = self.get_Qdelta_implicit(self.coll, qd_type=self.params.QI)
-
-    def integrate(self):
-        """
-        Integrates the right-hand side
-
-        Returns:
-            list of dtype_u: containing the integral as values
-        """
-
-        L = self.level
-        P = L.prob
-
-        me = []
-
-        # integrate RHS over all collocation nodes
-        for m in range(1, self.coll.num_nodes + 1):
-            # new instance of dtype_u, initialize values with 0
-            me.append(P.dtype_u(P.init, val=0.0))
-            for j in range(1, self.coll.num_nodes + 1):
-                me[-1] += L.dt * self.coll.Qmat[m, j] * L.f[j]
-
-        return me
 
     def update_nodes(self):
         """
@@ -55,50 +31,75 @@ class generic_implicit(sweeper):
         Returns:
             None
         """
-
-        L = self.level
-        P = L.prob
-
-        # only if the level has been touched before
-        assert L.status.unlocked
-
-        # get number of collocation nodes for easier access
         M = self.coll.num_nodes
 
-        # gather all terms which are known already (e.g. from the previous iteration)
-        # this corresponds to u0 + QF(u^k) - QdF(u^k) + tau
+        # only if the level has been touched before
+        assert self.level.status.unlocked
 
-        # get QF(u^k)
-        integral = self.integrate()
-        for m in range(M):
-            # get -QdF(u^k)_m
-            for j in range(1, M + 1):
-                integral[m] -= L.dt * self.QI[m + 1, j] * L.f[j]
+        rhs = self.build_right_hand_side()
 
-            # add initial value
-            integral[m] += L.u[0]
-            # add tau if associated
-            if L.tau[m] is not None:
-                integral[m] += L.tau[m]
-
-        # do the sweep
         for m in range(0, M):
-            # build rhs, consisting of the known values from above and new values from previous nodes (at k+1)
-            rhs = P.dtype_u(integral[m])
-            for j in range(1, m + 1):
-                rhs += L.dt * self.QI[m + 1, j] * L.f[j]
+            self.sweep(rhs, m)
 
-            # implicit solve with prefactor stemming from the diagonal of Qd
-            L.u[m + 1] = P.solve_system(
-                rhs, L.dt * self.QI[m + 1, m + 1], L.u[m + 1], L.time + L.dt * self.coll.nodes[m]
-            )
-            # update function values
-            L.f[m + 1] = P.eval_f(L.u[m + 1], L.time + L.dt * self.coll.nodes[m])
-
-        # indicate presence of new values at this level
-        L.status.updated = True
+        self.level.status.updated = True
 
         return None
+
+    def build_right_hand_side(self):
+        rhs = self.initialize_right_hand_side_buffer()
+        self.add_Q_minus_QD_times_F(rhs)
+        self.add_initial_conditions(rhs)
+        self.add_tau_correction(rhs)
+        return rhs
+
+    def initialize_right_hand_side_buffer(self):
+        problem = self.level.prob
+
+        return [problem.dtype_u(problem.init, val=0.0) for _ in range(self.coll.num_nodes)]
+
+    def add_Q_minus_QD_times_F(self, rhs):
+        self.add_matrix_times_f_evaluations_to(self.coll.Qmat - self.QI, rhs)
+
+    def add_matrix_times_f_evaluations_to(self, matrix, rhs):
+        for m in range(1, self.coll.num_nodes + 1):
+            for j in range(1, self.coll.num_nodes + 1):
+                rhs[m - 1] += self.level.dt * matrix[m, j] * self.level.f[j]
+
+    def add_initial_conditions(self, rhs):
+        for i in range(len(rhs)):
+            rhs[i] += self.level.u[0]
+
+    def add_tau_correction(self, rhs):
+        for m in range(self.coll.num_nodes):
+            if self.level.tau[m] is not None:
+                rhs[m] += self.level.tau[m]
+
+    def sweep(self, rhs, current_node):
+        L = self.level
+        P = L.prob
+        m = current_node
+
+        self.add_new_information_from_forward_substitution(rhs, m)
+
+        L.u[m + 1] = P.solve_system(
+            rhs[m], L.dt * self.QI[m + 1, m + 1], L.u[m + 1], L.time + L.dt * self.coll.nodes[m]
+        )
+        L.f[m + 1] = P.eval_f(L.u[m + 1], L.time + L.dt * self.coll.nodes[m])
+
+    def add_new_information_from_forward_substitution(self, rhs, current_node):
+        for j in range(1, current_node + 1):
+            rhs[current_node] += self.level.dt * self.QI[current_node + 1, j] * self.level.f[j]
+
+    def integrate(self):
+        """
+        Integrates the right-hand side
+
+        Returns:
+            list of dtype_u: containing the integral as values
+        """
+        rhs = self.initialize_right_hand_side_buffer()
+        self.add_matrix_times_f_evaluations_to(self.coll.Qmat, rhs)
+        return rhs
 
     def compute_end_point(self):
         """
