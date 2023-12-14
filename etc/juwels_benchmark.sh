6f06382db5b6d8488e3cdcf84905ff45c519085d
Jakob Fritz <j.fritz@fz-juelich.de>
2023-12-14 11:32:38 +0100

    Running on HPC-CI on Login-Node and submitting job

---
+++ etc/juwels_benchmark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+#SBATCH --account=${JUWELS_ACCOUNT}
+#SBATCH --nodes=1
+#SBATCH --time=00:10:00
+#SBATCH --partition=devel
+#SBATCH --export-file=sbatch.out
+#SBATCH --error=sbatch.err
+
+python -m pytest --continue-on-collection-errors -v pySDC/tests -m "benchmark" --benchmark-json=benchmarks/output.json
