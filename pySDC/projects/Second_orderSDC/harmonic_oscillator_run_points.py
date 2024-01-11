5b30c491f4f746f243279507b2df6c081b46af94
Ikrom Akramov <ikrom.akramov@tuhh.de>
2024-01-11 15:46:26 +0100

    Edited codes according to the comments in the GitHub

--- pySDC/projects/Second_orderSDC/harmonic_oscillator_run_points.py
+++ pySDC/projects/Second_orderSDC/harmonic_oscillator_run_points.py
@@ -1,18 +1,26 @@
 import numpy as np
-from pySDC.projects.Second_orderSDC.harmonic_oscillator_params import harmonic_oscillator_params
+from pySDC.projects.Second_orderSDC.harmonic_oscillator_params import get_default_harmonic_oscillator_description
 from pySDC.projects.Second_orderSDC.stability_simulation import compute_and_check_stability
 
+'''
+This script generates table for the given points to compare in what quadrature type,
+number of nodes and number of iterations the SDC iteration is stable or not.
+Additional parameter:
+    To save the table set: save_points_table=True
+    Change filename: points_table_filename='FILENAME' by default: './data/point_table.txt'
+'''
 if __name__ == '__main__':
-    # Define lists for the number of nodes and maximum iterations
-    description = harmonic_oscillator_params()
+    # Get default parameters for the harmonic osicillator problem
+    description = get_default_harmonic_oscillator_description()
+    # Additonal params to compute stability points
     helper_params = {
         'quad_type_list': ('GAUSS', 'LOBATTO'),
         'Num_iter': (2, 2),
         'num_nodes_list': np.arange(3, 6, 1),
         'max_iter_list': np.arange(2, 10, 1),
     }
-    description['helper_params'] = helper_params
+
     points = ((1, 100), (3, 100), (10, 100))
     # Iterate through points and perform stability check
     for ii in points:
-        compute_and_check_stability(description, ii, check_stability=True)
+        compute_and_check_stability(description, helper_params, ii, check_stability=True)
--- pySDC/projects/Second_orderSDC/stability_simulation.py
+++ pySDC/projects/Second_orderSDC/stability_simulation.py
@@ -4,12 +4,21 @@ from pySDC.core.Errors import ProblemError
 from pySDC.core.Step import step
 
 from pySDC.projects.Second_orderSDC.plot_helper import set_fixed_plot_params
-from tabulate import tabulate
 
 
 class StabilityImplementation:
     """
-    Routine to compute the stability domains of different configurations of SDC
+    This class computes and implements stability region of the harmonic oscillator problem
+    by using different methods (SDC, Picard, RKN).
+
+    Parameters
+    -----------
+    description: gets default paramets for the problem class
+    kappa_max: maximum value of kappa can reach
+    mu_max: maximum value of mu can reach
+    Num_iter: maximum iterations for the kappa and mu on the x and y axes
+    cwd: current working
+
     """
 
     def __init__(self, description, kappa_max=20, mu_max=20, Num_iter=(400, 400), cwd=''):
@@ -37,58 +46,67 @@ class StabilityImplementation:
             numpy.ndarray: domain_Kpicard
         """
         S = step(description=self.description)
-
+        # Define L to get access level params and functions
         L = S.levels[0]
+        # Number of nodes
+        num_nodes = L.sweep.coll.num_nodes
+        # Time step
+        dt = L.params.dt
 
+        # Define Collocation matrix to find for the stability function
         Q = L.sweep.coll.Qmat[1:, 1:]
         QQ = np.dot(Q, Q)
-        num_nodes = L.sweep.coll.num_nodes
-        dt = L.params.dt
         Q_coll = np.block([[QQ, np.zeros([num_nodes, num_nodes])], [np.zeros([num_nodes, num_nodes]), Q]])
         qQ = np.dot(L.sweep.coll.weights, Q)
-
+        # Matrix with all entries 1
         ones = np.block([[np.ones(num_nodes), np.zeros(num_nodes)], [np.zeros(num_nodes), np.ones(num_nodes)]])
-
+        # Combine all of the weights into a single matrix
         q_mat = np.block(
             [
                 [dt**2 * qQ, np.zeros(num_nodes)],
                 [np.zeros(num_nodes), dt * L.sweep.coll.weights],
             ]
         )
-
+        # Zeros matrices to store the values for the stability region values
         domain_SDC = np.zeros((self.kappa_iter, self.mu_iter), dtype="complex")
         domain_picard = np.zeros((self.kappa_iter, self.mu_iter))
         domain_Ksdc = np.zeros((self.kappa_iter, self.mu_iter))
         domain_Kpicard = np.zeros((self.kappa_iter, self.mu_iter))
+        # Loop over the different values of the kappa and mu values
         for i in range(0, self.kappa_iter):
             for j in range(0, self.mu_iter):
                 k = self.lambda_kappa[i]
                 mu = self.lambda_mu[j]
+                # Build right hand side matrix function for the harmonic oscillator problem
                 F = np.block(
                     [
                         [-k * np.eye(num_nodes), -mu * np.eye(num_nodes)],
                         [-k * np.eye(num_nodes), -mu * np.eye(num_nodes)],
                     ]
                 )
+
                 if self.K_iter != 0:
+                    # num iteration is not equal to zero then do SDC and Picard iteration
                     lambdas = [k, mu]
                     SDC_mat_sweep, Ksdc_eigval = L.sweep.get_scalar_problems_manysweep_mats(
                         nsweeps=self.K_iter, lambdas=lambdas
                     )
+                    # If picard_mats_sweep=True then do also Picard iteration
                     if L.sweep.params.picard_mats_sweep:
                         (
                             Picard_mat_sweep,
                             Kpicard_eigval,
                         ) = L.sweep.get_scalar_problems_picardsweep_mats(nsweeps=self.K_iter, lambdas=lambdas)
                     else:
-                        ProblemError("Picard interation is False")
+                        ProblemError("Picard iteration is not enabled. Set 'picard_mats_sweep' to True to enable.")
                     domain_Ksdc[i, j] = Ksdc_eigval
                     if L.sweep.params.picard_mats_sweep:
                         domain_Kpicard[i, j] = Kpicard_eigval
 
                 else:
+                    # Otherwise Collocation problem
                     SDC_mat_sweep = np.linalg.inv(np.eye(2 * num_nodes) - dt * np.dot(Q_coll, F))
-
+                # Collation update for both Picard and SDC iterations
                 if L.sweep.params.do_coll_update:
                     FSDC = np.dot(F, SDC_mat_sweep)
                     Rsdc_mat = np.array([[1.0, dt], [0, 1.0]]) + np.dot(q_mat, FSDC) @ ones.T
@@ -99,9 +117,8 @@ class StabilityImplementation:
                         Rpicard_mat = np.array([[1.0, dt], [0, 1.0]]) + np.dot(q_mat, FPicard) @ ones.T
                         stab_func_picard, v = np.linalg.eig(Rpicard_mat)
                 else:
-                    pass
-                    raise ProblemError("Collocation update step is only works for True")
-
+                    raise ProblemError("Collocation update step works only when 'do_coll_update' is set to True.")
+                # Find and store spectral radius
                 domain_SDC[i, j] = np.max(np.abs(stab_func))
                 if L.sweep.params.picard_mats_sweep:
                     domain_picard[i, j] = np.max(np.abs(stab_func_picard))
@@ -213,23 +230,26 @@ class StabilityImplementation:
 
 def compute_and_check_stability(
     description,
+    helper_params,
     point,
     compute_interval=False,
     save_interval_file=False,
     interval_filename='./data/stab_interval.txt',
     check_stability=False,
-    store_results=False,
-    results_filename='./data/stab_results.txt',
+    save_points_table=False,
+    points_table_filename='./data/point_table.txt',
     quadrature_list=('GAUSS', 'LOBATTO'),
 ):
+    from tabulate import tabulate
+
     # Storage for stability interval and stability check
     interval_data = []
     results_data = []
 
     # Loop through different numbers of nodes and maximum iterations
-    for num_nodes in description['helper_params']['num_nodes_list']:
-        for max_iter in description['helper_params']['max_iter_list']:
-            for quad_type in description['helper_params']['quad_type_list']:
+    for num_nodes in helper_params['num_nodes_list']:
+        for max_iter in helper_params['max_iter_list']:
+            for quad_type in helper_params['quad_type_list']:
                 # Update simulation parameters
                 description['sweeper_params']['num_nodes'] = num_nodes
                 description['sweeper_params']['quad_type'] = quad_type
@@ -238,7 +258,7 @@ def compute_and_check_stability(
                 # Create Stability_implementation instance for stability check
 
                 stab_model = StabilityImplementation(
-                    description, kappa_max=point[0], mu_max=point[1], Num_iter=description['helper_params']['Num_iter']
+                    description, kappa_max=point[0], mu_max=point[1], Num_iter=helper_params['Num_iter']
                 )
                 if compute_interval:
                     # Extract the values where SDC is less than or equal to 1
@@ -277,11 +297,11 @@ def compute_and_check_stability(
             file.write(interval_table_str)
         print(f"Stability Interval Table saved to {interval_filename}")
 
-    if store_results and check_stability:
+    if save_points_table and check_stability:
         results_table_str = tabulate(results_data, headers=results_headers, tablefmt="grid")
-        with open(results_filename, 'w') as file:
+        with open(points_table_filename, 'w') as file:
             file.write(results_table_str)
-        print(f"Stability Results Table saved to {results_filename}")
+        print(f"Stability Results Table saved to {points_table_filename}")
 
     if compute_interval:
         print("Stability Interval Table:")
