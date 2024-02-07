4f59a29ae6aec2e65164fc5c7d4a2ca501d0c22f
Ikrom Akramov <96234984+ikrom96git@users.noreply.github.com>
2024-02-07 11:08:19 +0100

    Bug fix (#396)
    * Clear documentation and some edits in the code
    
    * forgot black
    
    * some changes

--- pySDC/projects/Second_orderSDC/harmonic_oscillator_run_stab_interval.py
+++ pySDC/projects/Second_orderSDC/harmonic_oscillator_run_stab_interval.py
@@ -2,28 +2,40 @@ import numpy as np
 from pySDC.projects.Second_orderSDC.harmonic_oscillator_params import get_default_harmonic_oscillator_description
 from pySDC.projects.Second_orderSDC.stability_simulation import compute_and_generate_table
 
-
 if __name__ == '__main__':
     '''
-    To compute maximum stable values for SDC and Picard iterations for the purely oscillatory case with
-    no damping (mu=0)
-    Additional parameter:
-        To save the table set: save_interval_file=True
-        Change filename: interval_filename='FILENAME' by default: './data/stab_interval.txt'
+    Main script to compute maximum stable values for SDC and Picard iterations for the purely oscillatory case with no damping (mu=0).
+
+    Additional parameters in the `compute_and_generate_table` function:
+        - To save the stability table, set `save_interval_file=True`.
+        - To change the filename, set `interval_filename='FILENAME'`. Default is './data/stab_interval.txt'.
+
     Output:
-        it generates to compare with different values of M (number of nodes) and K (maximal number of iterations)
+        The script generates data to compare different values of M (number of nodes) and K (maximal number of iterations).
     '''
-    # Ger default parameters
+    # This code checks if the "data" folder exists or not.
+    exec(open("check_data_folder.py").read())
+    # Get default parameters for the harmonic oscillator
     description = get_default_harmonic_oscillator_description()
+
     # Additional parameters to compute stability interval on the kappa
+    # =============================================================================
+    #     To get exactly the same as table in the paper set:
+    #       'Num_iter': (500, 1) for the SDC iteration
+    #       'Num_iter': (2000, 1) for the Picard iteration
+    # =============================================================================
     helper_params = {
-        'quad_type_list': ('GAUSS',),
-        'Num_iter': (2000, 1),
-        'num_nodes_list': np.arange(2, 7, 1),
-        'max_iter_list': np.arange(1, 11, 1),
+        'quad_type_list': ('GAUSS',),  # Type of quadrature
+        'Num_iter': (500, 1),  # Number of iterations
+        'num_nodes_list': np.arange(2, 7, 1),  # List of number of nodes
+        'max_iter_list': np.arange(1, 11, 1),  # List of maximum iterations
     }
 
-    points = ((100, 1e-10),)
+    points = ((100, 1e-10),)  # Stability parameters: (Num_iter, mu)
+
     # Iterate through points and perform stability check
     for ii in points:
-        compute_and_generate_table(description, helper_params, ii, compute_interval=True)
+        # If you want to get the table for the Picard iteration set Picard=True
+        compute_and_generate_table(
+            description, helper_params, ii, compute_interval=True, Picard=False, save_interval_file=True
+        )
