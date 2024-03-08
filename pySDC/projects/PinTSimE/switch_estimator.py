82a6b739af533c5635826f35e23e0e54458e4fc0
Lisa Wimmer <68507897+lisawim@users.noreply.github.com>
2024-03-08 13:21:21 +0100

    `pySDC`-build-in `LagrangeApproximation` class in `SwitchEstimator` (#406)
    * SE now uses LagrangeApproximation class + removed Lagrange class in SE
    
    * Removed log message again (not corresponding to PR)

--- pySDC/projects/PinTSimE/switch_estimator.py
+++ pySDC/projects/PinTSimE/switch_estimator.py
@@ -4,6 +4,7 @@ import scipy as sp
 from pySDC.core.Collocation import CollBase
 from pySDC.core.ConvergenceController import ConvergenceController, Status
 from pySDC.implementations.convergence_controller_classes.check_convergence import CheckConvergence
+from pySDC.core.Lagrange import LagrangeApproximation
 
 
 class SwitchEstimator(ConvergenceController):
@@ -274,23 +275,8 @@ class SwitchEstimator(ConvergenceController):
            Time point of found event.
         """
 
-        LagrangeInterpolator = LagrangeInterpolation(t_interp, state_function)
-
-        def p(t):
-            """
-            Simplifies the call of the interpolant.
-
-            Parameters
-            ----------
-            t : float
-                Time t at which the interpolant is called.
-
-            Returns
-            -------
-            p(t) : float
-                The value of the interpolated function at time t.
-            """
-            return LagrangeInterpolator.eval(t)
+        LagrangeInterpolation = LagrangeApproximation(points=t_interp, fValues=state_function)
+        p = lambda t: LagrangeInterpolation.__call__(t)
 
         def fprime(t):
             """
@@ -385,47 +371,3 @@ def newton(x0, p, fprime, newton_tol, newton_maxiter):
     root = x0
 
     return root
-
-
-class LagrangeInterpolation(object):
-    def __init__(self, ti, yi):
-        """Initialization routine"""
-        self.ti = np.asarray(ti)
-        self.yi = np.asarray(yi)
-        self.n = len(ti)
-
-    def get_Lagrange_polynomial(self, t, i):
-        """
-        Computes the basis of the i-th Lagrange polynomial.
-
-        Parameters
-        ----------
-        t : float
-            Time where the polynomial is computed at.
-        i : int
-            Index of the Lagrange polynomial
-
-        Returns
-        -------
-        product : float
-            The product of the bases.
-        """
-        product = np.prod([(t - self.ti[k]) / (self.ti[i] - self.ti[k]) for k in range(self.n) if k != i])
-        return product
-
-    def eval(self, t):
-        """
-        Evaluates the Lagrange interpolation at time t.
-
-        Parameters
-        ----------
-        t : float
-            Time where interpolation is computed.
-
-        Returns
-        -------
-        p : float
-            Value of interpolant at time t.
-        """
-        p = np.sum([self.yi[i] * self.get_Lagrange_polynomial(t, i) for i in range(self.n)])
-        return p
