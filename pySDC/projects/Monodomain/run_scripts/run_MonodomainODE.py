84820ec11e51c2f3496ba96b531c04b48204cc87
Giacomo <jackrosilho@gmail.com>
2024-03-13 06:04:22 +0100

    added few comments, cleaned a bit

--- pySDC/projects/Monodomain/run_scripts/run_MonodomainODE.py
+++ pySDC/projects/Monodomain/run_scripts/run_MonodomainODE.py
@@ -18,6 +18,8 @@ from pySDC.projects.Monodomain.controller_classes.my_controller_nonMPI import my
 from pySDC.projects.Monodomain.sweeper_classes.exponential_runge_kutta.imexexp_1st_order import imexexp_1st_order as imexexp_1st_order_ExpRK
 from pySDC.projects.Monodomain.sweeper_classes.runge_kutta.imexexp_1st_order import imexexp_1st_order
 
+from pySDC.projects.Monodomain.transfer_classes.TransferVectorOfDCTVectors import TransferVectorOfDCTVectors
+
 
 def set_logger(controller_params):
     logging.basicConfig(level=controller_params["logger_level"])
@@ -83,7 +85,7 @@ def get_controller_params(problem_params, n_time_ranks):
     return controller_params
 
 
-def get_description(integrator, problem_params, sweeper_params, level_params, step_params, base_transfer_params, space_transfer_class, space_transfer_params):
+def get_description(integrator, problem_params, sweeper_params, level_params, step_params, base_transfer_params, space_transfer_class):
     description = dict()
 
     problem = MultiscaleMonodomainODE
@@ -102,7 +104,7 @@ def get_description(integrator, problem_params, sweeper_params, level_params, st
     description["step_params"] = step_params
     description["base_transfer_params"] = base_transfer_params
     description["space_transfer_class"] = space_transfer_class
-    description["space_transfer_params"] = space_transfer_params
+
     return description
 
 
@@ -137,44 +139,34 @@ def get_sweeper_params(num_nodes, skip_residual_computation):
 
 def get_space_tranfer_params():
 
-    from pySDC.projects.Monodomain.transfer_classes.TransferVectorOfDCTVectors import TransferVectorOfDCTVectors
-
     space_transfer_class = TransferVectorOfDCTVectors
-    space_transfer_params = dict()
 
-    return space_transfer_class, space_transfer_params
+    return space_transfer_class
 
 
-def get_problem_params(
-    domain_name,
-    refinements,
-    ionic_model_name,
-    read_init_val,
-    init_time,
-    enable_output,
-    end_time,
-    order,
-    output_root,
-    output_file_name,
-    ref_sol,
-):
+def get_problem_params(domain_name, refinements, ionic_model_name, read_init_val, init_time, enable_output, end_time, order, output_root, output_file_name, ref_sol):
     # initialize problem parameters
     problem_params = dict()
-    problem_params["order"] = order
-    problem_params["refinements"] = refinements
+    problem_params["order"] = order  # order of the spatial discretization
+    problem_params["refinements"] = refinements  # number of refinements with respect to a baseline
+    problem_params["domain_name"] = domain_name  # name of the domain: cube_1D, cube_2D, cube_3D, cuboid_1D, cuboid_2D, cuboid_3D, cuboid_1D_small, cuboid_2D_small, cuboid_3D_small
+    problem_params["ionic_model_name"] = (
+        ionic_model_name  # name of the ionic model: HH, CRN, TTP, TTP_SMOOTH for Hodgkin-Huxley, Courtemanche-Ramirez-Nattel, Ten Tusscher-Panfilov and a smoothed version of Ten Tusscher-Panfilov
+    )
+    problem_params["read_init_val"] = read_init_val  # read the initial value from file (True) or initiate an action potential with a stimulus (False)
+    problem_params["init_time"] = init_time  # stimulus happpens at t=0 and t=1000 and lasts 2ms. If init_time>2 nothing happens up to t=1000. If init_time>1002 nothing happens, never.
+    problem_params["init_val_name"] = "init_val_DCT"  # name of the file containing the initial value
+    problem_params["enable_output"] = enable_output  # activate or deactivate output (that can be visualized with visualization/show_monodomain_sol.py)
+    problem_params["output_V_only"] = True  # output only the transmembrane potential (V) and not the ionic model variables
     executed_file_dir = os.path.dirname(os.path.realpath(__file__))
-    problem_params["domain_name"] = domain_name
-    problem_params["ionic_model_name"] = ionic_model_name
-    problem_params["read_init_val"] = read_init_val
-    problem_params["init_time"] = init_time
-    problem_params["init_val_name"] = "init_val_DCT"
-    problem_params["enable_output"] = enable_output
-    problem_params["output_V_only"] = True
-    problem_params["output_root"] = executed_file_dir + "/../../../../data/Monodomain/" + output_root
+    problem_params["output_root"] = (
+        executed_file_dir + "/../output_data/" + output_root
+    )  # output root folder. A hierarchy of folders is created in this folder, as root/domain_name/ref_+str(refinements)/ionic_model_name. Initial values are put here
     problem_params["output_file_name"] = output_file_name
-    problem_params["ref_sol"] = ref_sol
+    problem_params["ref_sol"] = ref_sol  # reference solution file name
     problem_params["end_time"] = end_time
     Path(problem_params["output_root"]).mkdir(parents=True, exist_ok=True)
+
     return problem_params
 
 
@@ -202,10 +194,12 @@ def setup_and_run(
     n_time_ranks,
     finter,
 ):
-    # get space-time communicators
+
+    # get time communicator
     time_comm, time_rank = get_comms(n_time_ranks, truly_time_parallel)
+
     # get time integration parameters
-    # set maximum number of iterations in SDC/ESDC/MLSDC/etc
+    # set maximum number of iterations in ESDC/MLESDC/PFASST
     step_params = get_step_params(maxiter=max_iter)
     # set number of collocation nodes in each level
     sweeper_params = get_sweeper_params(num_nodes=num_nodes, skip_residual_computation=skip_residual_computation)
@@ -215,6 +209,10 @@ def setup_and_run(
         nsweeps=num_sweeps,
         restol=restol,
     )
+
+    # fix enable output to that only finest level has output
+    n_levels = max(len(refinements), len(num_nodes))
+    enable_output = [enable_output] + [False] * (n_levels - 1)
     # get problem parameters
     problem_params = get_problem_params(
         domain_name=domain_name,
@@ -230,44 +228,41 @@ def setup_and_run(
         ref_sol=ref_sol,
     )
 
-    space_transfer_class, space_transfer_params = get_space_tranfer_params()
+    space_transfer_class = get_space_tranfer_params()
 
-    # Usually do not modify below this line ------------------
     # get remaining prams
     base_transfer_params = get_base_transfer_params(finter)
     controller_params = get_controller_params(problem_params, n_time_ranks)
-    description = get_description(integrator, problem_params, sweeper_params, level_params, step_params, base_transfer_params, space_transfer_class, space_transfer_params)
+    description = get_description(integrator, problem_params, sweeper_params, level_params, step_params, base_transfer_params, space_transfer_class)
     set_logger(controller_params)
     controller = get_controller(controller_params, description, time_comm, n_time_ranks, truly_time_parallel)
 
     # get PDE data
     t0, Tend, uinit, P = get_P_data(controller, truly_time_parallel)
 
-    # print dofs stats (dofs per processor, etc.)
+    # print dofs stats
     print_dofs_stats(time_rank, controller, P, uinit)
 
-    # call main function to get things done...
+    # run
     uend, stats = controller.run(u0=uinit, t0=t0, Tend=Tend)
 
+    # write reference solution, to be used later for error computation
     if write_as_reference_solution:
         P.write_reference_solution(uend, Tend)
 
-    # compute errors, if a reference solution is available
+    # compute errors, if a reference solution is found
     error_availabe, error_L2, rel_error_L2 = P.compute_errors(uend)
 
     # get some stats
     iter_counts = get_sorted(stats, type="niter", sortby="time")
     niters = [item[1] for item in iter_counts]
+    # gather stats
     if time_comm is not None:
         niters = time_comm.gather(niters, root=0)
-        if time_comm.rank == 0:
-            niters = np.array(niters).flatten()
-            avg_niters = np.mean(niters)
-        else:
-            avg_niters = 0
-        avg_niters = time_comm.bcast(avg_niters, root=0)
-
-    if time_comm is None or time_comm.rank == 0:
+        niters = np.array(niters).flatten()  # only rank 0 is doing something meaningful
+        niters = time_comm.bcast(niters, root=0)
+    avg_niters = np.mean(niters)
+    if time_rank == 0:
         controller.logger.info("Mean number of iterations: %4.2f" % avg_niters)
         controller.logger.info("Std and var for number of iterations: %4.2f -- %4.2f" % (float(np.std(niters)), float(np.var(niters))))
 
@@ -291,7 +286,7 @@ def main():
     # set problem parameters
     domain_name = "cube_1D"
     refinements = [0]
-    order = 4
+    order = 4  # 2 or 4
     ionic_model_name = "TTP"
     read_init_val = False
     init_time = 0.0
