ea1ed4844206ea4d01a8142e7f9f70ac51951ab8
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2024-04-01 13:47:23 +0200

    Generic MPI FFT class (#408)
    * Added generic MPIFFT problem class
    
    * Fixes
    
    * Generalized to `xp` in preparation for GPUs
    
    * Fixes
    
    * Ported Allen-Cahn to generic MPI FFT implementation

--- pySDC/implementations/problem_classes/NonlinearSchroedinger_MPIFFT.py
+++ pySDC/implementations/problem_classes/NonlinearSchroedinger_MPIFFT.py
@@ -1,18 +1,14 @@
 import numpy as np
-from scipy.optimize import newton_krylov, root
+from scipy.optimize import newton_krylov
 from scipy.optimize.nonlin import NoConvergence
-import scipy.sparse as sp
-from mpi4py import MPI
-from mpi4py_fft import PFFT
 
 from pySDC.core.Errors import ProblemError
-from pySDC.core.Problem import ptype, WorkCounter
-from pySDC.implementations.datatype_classes.mesh import mesh, imex_mesh
+from pySDC.core.Problem import WorkCounter
+from pySDC.implementations.problem_classes.generic_MPIFFT_Laplacian import IMEX_Laplacian_MPIFFT
+from pySDC.implementations.datatype_classes.mesh import mesh
 
-from mpi4py_fft import newDistArray
 
-
-class nonlinearschroedinger_imex(ptype):
+class nonlinearschroedinger_imex(IMEX_Laplacian_MPIFFT):
     r"""
     Example implementing the :math:`N`-dimensional nonlinear Schrödinger equation with periodic boundary conditions
 
@@ -51,130 +47,17 @@ class nonlinearschroedinger_imex(ptype):
         Journal of Parallel and Distributed Computing (2019).
     """
 
-    dtype_u = mesh
-    dtype_f = imex_mesh
-
-    def __init__(self, nvars=None, spectral=False, L=2 * np.pi, c=1.0, comm=MPI.COMM_WORLD):
+    def __init__(self, c=1.0, **kwargs):
         """Initialization routine"""
-
-        if nvars is None:
-            nvars = (128, 128)
-
-        if not L == 2.0 * np.pi:
-            raise ProblemError(f'Setup not implemented, L has to be 2pi, got {L}')
+        super().__init__(L=2 * np.pi, alpha=1j, dtype='D', **kwargs)
 
         if not (c == 0.0 or c == 1.0):
             raise ProblemError(f'Setup not implemented, c has to be 0 or 1, got {c}')
+        self._makeAttributeAndRegister('c', localVars=locals(), readOnly=True)
 
-        if not (isinstance(nvars, tuple) and len(nvars) > 1):
-            raise ProblemError('Need at least two dimensions')
-
-        # Creating FFT structure
-        self.ndim = len(nvars)
-        axes = tuple(range(self.ndim))
-        self.fft = PFFT(comm, list(nvars), axes=axes, dtype=np.complex128, collapse=True)
-
-        # get test data to figure out type and dimensions
-        tmp_u = newDistArray(self.fft, spectral)
-
-        L = np.array([L] * self.ndim, dtype=float)
-
-        # invoke super init, passing the communicator and the local dimensions as init
-        super(nonlinearschroedinger_imex, self).__init__(init=(tmp_u.shape, comm, tmp_u.dtype))
-        self._makeAttributeAndRegister('nvars', 'spectral', 'L', 'c', 'comm', localVars=locals(), readOnly=True)
-
-        # get local mesh
-        X = np.ogrid[self.fft.local_slice(False)]
-        N = self.fft.global_shape()
-        for i in range(len(N)):
-            X[i] = X[i] * self.L[i] / N[i]
-        self.X = [np.broadcast_to(x, self.fft.shape(False)) for x in X]
-
-        # get local wavenumbers and Laplace operator
-        s = self.fft.local_slice()
-        N = self.fft.global_shape()
-        k = [np.fft.fftfreq(n, 1.0 / n).astype(int) for n in N]
-        K = [ki[si] for ki, si in zip(k, s)]
-        Ks = np.meshgrid(*K, indexing='ij', sparse=True)
-        Lp = 2 * np.pi / self.L
-        for i in range(self.ndim):
-            Ks[i] = (Ks[i] * Lp[i]).astype(float)
-        K = [np.broadcast_to(k, self.fft.shape(True)) for k in Ks]
-        K = np.array(K).astype(float)
-        self.K2 = np.sum(K * K, 0, dtype=float)
-
-        # Need this for diagnostics
-        self.dx = self.L / nvars[0]
-        self.dy = self.L / nvars[1]
-
-        # work counters
-        self.work_counters['rhs'] = WorkCounter()
-
-    def eval_f(self, u, t):
-        """
-        Routine to evaluate the right-hand side of the problem.
-
-        Parameters
-        ----------
-        u : dtype_u
-            Current values of the numerical solution.
-        t : float
-            Current time at which the numerical solution is computed.
-
-        Returns
-        -------
-        f : dtype_f
-            The right-hand side of the problem.
-        """
-
-        f = self.dtype_f(self.init)
-
-        if self.spectral:
-            f.impl = -self.K2 * 1j * u
-            tmp = self.fft.backward(u)
-            tmpf = self.ndim * self.c * 2j * np.absolute(tmp) ** 2 * tmp
-            f.expl[:] = self.fft.forward(tmpf)
-
-        else:
-            u_hat = self.fft.forward(u)
-            lap_u_hat = -self.K2 * 1j * u_hat
-            f.impl[:] = self.fft.backward(lap_u_hat, f.impl)
-            f.expl = self.ndim * self.c * 2j * np.absolute(u) ** 2 * u
-
-        self.work_counters['rhs']()
-        return f
-
-    def solve_system(self, rhs, factor, u0, t):
-        """
-        Simple FFT solver for the diffusion part.
-
-        Parameters
-        ----------
-        rhs : dtype_f
-            Right-hand side for the linear system.
-        factor : float
-            Abbrev. for the node-to-node stepsize (or any other factor required).
-        u0 : dtype_u
-            Initial guess for the iterative solver (not used here so far).
-        t : float
-            Current time (e.g. for time-dependent BCs).
-
-        Returns
-        -------
-        me : dtype_u
-            The solution as mesh.
-        """
-
-        if self.spectral:
-            me = rhs / (1.0 + factor * self.K2 * 1j)
-
-        else:
-            me = self.dtype_u(self.init)
-            rhs_hat = self.fft.forward(rhs)
-            rhs_hat /= 1.0 + factor * self.K2 * 1j
-            me[:] = self.fft.backward(rhs_hat)
-
-        return me
+    def _eval_explicit_part(self, u, t, f_expl):
+        f_expl[:] = self.ndim * self.c * 2j * self.xp.absolute(u) ** 2 * u
+        return f_expl
 
     def u_exact(self, t, **kwargs):
         r"""
@@ -198,9 +81,9 @@ class nonlinearschroedinger_imex(ptype):
         def nls_exact_1D(t, x, c):
             ae = 1.0 / np.sqrt(2.0) * np.exp(1j * t)
             if c != 0:
-                u = ae * ((np.cosh(t) + 1j * np.sinh(t)) / (np.cosh(t) - 1.0 / np.sqrt(2.0) * np.cos(x)) - 1.0)
+                u = ae * ((np.cosh(t) + 1j * np.sinh(t)) / (np.cosh(t) - 1.0 / np.sqrt(2.0) * self.xp.cos(x)) - 1.0)
             else:
-                u = np.sin(x) * np.exp(-t * 1j)
+                u = self.xp.sin(x) * np.exp(-t * 1j)
 
             return u
 
@@ -261,13 +144,13 @@ class nonlinearschroedinger_fully_implicit(nonlinearschroedinger_imex):
 
         if self.spectral:
             tmp = self.fft.backward(u)
-            tmpf = self.ndim * self.c * 2j * np.absolute(tmp) ** 2 * tmp
+            tmpf = self.ndim * self.c * 2j * self.xp.absolute(tmp) ** 2 * tmp
             f[:] = -self.K2 * 1j * u + self.fft.forward(tmpf)
 
         else:
             u_hat = self.fft.forward(u)
             lap_u_hat = -self.K2 * 1j * u_hat
-            f[:] = self.fft.backward(lap_u_hat) + self.ndim * self.c * 2j * np.absolute(u) ** 2 * u
+            f[:] = self.fft.backward(lap_u_hat) + self.ndim * self.c * 2j * self.xp.absolute(u) ** 2 * u
 
         self.work_counters['rhs']()
         return f
