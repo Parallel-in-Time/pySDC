e40f68d606e6403b810aab2297aa8c23cd289e81
Jakob Fritz <j.fritz@fz-juelich.de>
2024-05-03 18:19:43 +0200

    Reformatted

--- pySDC/projects/parallelSDC/tests/test_preconditioner_MPI.py
+++ pySDC/projects/parallelSDC/tests/test_preconditioner_MPI.py
@@ -6,7 +6,7 @@ import pytest
 @pytest.mark.slow
 @pytest.mark.mpi4py
 @pytest.mark.timeout(0)
-@pytest.mark.parametrize('num_procs', [3,5])
+@pytest.mark.parametrize('num_procs', [3, 5])
 def test_preconditioner_playground_MPI(num_procs):
     # Set python path once
     my_env = os.environ.copy()
