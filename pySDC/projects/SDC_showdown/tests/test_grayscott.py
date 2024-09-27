5b571dde88b704a28b8f79cbf3214653c8d28a64
Robert Speck <pancetta@users.noreply.github.com>
2024-09-27 10:35:18 +0200

    Update test_grayscott.py

--- pySDC/projects/SDC_showdown/tests/test_grayscott.py
+++ pySDC/projects/SDC_showdown/tests/test_grayscott.py
@@ -1,8 +1,8 @@
 import pytest
 
-
+@pytest.mark.slow
 @pytest.mark.petsc
-@pytest.mark.timeout(600)
+@pytest.mark.timeout(900)
 def test_grayscott():
     from pySDC.projects.SDC_showdown.SDC_timing_GrayScott import main
 
