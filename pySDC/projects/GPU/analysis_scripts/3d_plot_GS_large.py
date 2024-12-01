ff56864d0ae163efd900a1afe76f416da0b0507b
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2024-12-01 11:34:01 +0100

    Changed plotting a bit

--- pySDC/projects/GPU/analysis_scripts/3d_plot_GS_large.py
+++ pySDC/projects/GPU/analysis_scripts/3d_plot_GS_large.py
@@ -8,25 +8,26 @@ import gc
 def plot(n_time, n_space, useGPU, n_frames, base_path, space_range, res, start_frame=0):
     for i in range(start_frame, n_frames):
         p = pv.Plotter(off_screen=True)
-    
-        v = None
+
+        _v = None
         gc.collect()
         for procs in space_range:
             gc.collect()
-    
+
             path = f'{base_path}/GrayScottLarge-res_{res}-useGPU_{useGPU}-procs_1_{n_time}_{n_space}-0-{procs}-solution_{i:06d}.pickle'
             with open(path, 'rb') as file:
                 _data = pickle.load(file)
-    
-            if v is None:
-                v = np.zeros(_data['shape'])
-    
-            v[*_data['local_slice']] = _data['v']
-    
-        v = pv.wrap(v)
-    
+
+            if _v is None:
+                _v = np.zeros(_data['shape'])
+
+            _v[*_data['local_slice']] = _data['v']
+
+        v = pv.wrap(_v)
+
+        # plot whole thing
         contours = v.contour(isosurfaces=[0.3])
-        p.add_mesh(contours, opacity=0.9, cmap=['teal'])
+        p.add_mesh(contours, opacity=0.7, cmap=['teal'])
         p.remove_scalar_bar()
         p.camera.azimuth += 15
 
@@ -38,11 +39,26 @@ def plot(n_time, n_space, useGPU, n_frames, base_path, space_range, res, start_f
         p.screenshot(path, window_size=(4096, 4096))
         print(f'Saved {path}', flush=True)
 
-        for view in ['xy', 'xz', 'yz']:
-            path = f'{plotting_path}/GS_large_{view}_{i:06d}.png'
-            p.camera.tight(view=view)
-            p.screenshot(path, window_size=(4096, 4096))
-            print(f'Saved {path}', flush=True)
+        # for view in ['xy', 'xz', 'yz']:
+        #     path = f'{plotting_path}/GS_large_{view}_{i:06d}.png'
+        #     p.camera.tight(view=view)
+        #     p.screenshot(path, window_size=(4096, 4096))
+        #     print(f'Saved {path}', flush=True)
+
+        # plot slice
+        v_slice = pv.wrap(_v[:10, ...])
+        contours = v_slice.contour(isosurfaces=[0.3])
+        p.add_mesh(contours, opacity=0.7, cmap=['teal'])
+        p.remove_scalar_bar()
+        p.camera.azimuth += 15
+
+        p.camera.Elevation(0.7)
+        plotting_path = './simulation_plots/'
+
+        path = f'{plotting_path}/GS_large_slice_{i:06d}.png'
+        p.camera.tight(view='yz')
+        p.screenshot(path, window_size=(4096, 4096))
+        print(f'Saved {path}', flush=True)
 
     print('done')
 
@@ -51,13 +67,17 @@ def video(view=None):
     if view is None:
         path = 'simulation_plots/GS_large_%06d.png'
         path_target = 'videos/GS_large.mp4'
+    elif view == 'slice':
+        path = 'simulation_plots/GS_large_slice_%06d.png'
+        path_target = 'videos/GS_large_slice.mp4'
     else:
         path = f'simulation_plots/GS_large_{view}_%06d.png'
         path_target = f'videos/GS_large_{view}.mp4'
-    
+
     cmd = f'ffmpeg -i {path} -pix_fmt yuv420p -r 9 -s 2048:1536 -y {path_target}'.split()
-    
+
     subprocess.run(cmd)
+
 
 if __name__ == '__main__':
     import argparse
@@ -67,6 +87,7 @@ if __name__ == '__main__':
     parser.add_argument('--mode', type=str, choices=['plot', 'video'], default='plot')
     parser.add_argument('--nframes', type=int, default=100)
     parser.add_argument('--start', type=int, default=0)
+    parser.add_argument('--base_path', type=str, default='/p/scratch/ccstma/baumann7/large_runs/data/')
     args = parser.parse_args()
 
     from pySDC.projects.GPU.analysis_scripts.large_simulations import GSLarge
@@ -80,15 +101,23 @@ if __name__ == '__main__':
     else:
         raise NotImplementedError()
 
-    base_path = '/p/scratch/ccstma/baumann7/large_runs/data/'
-
     if args.mode == 'plot':
-        pv.start_xvfb()
-        plot(n_time = sim.params['procs'][1], n_space=sim.params['procs'][2], useGPU=sim.params['useGPU'], base_path=base_path, space_range=range(sim.params['procs'][2]), n_frames=args.nframes, res=sim.params['res'], start_frame=args.start)
+        try:
+            pv.start_xvfb()
+        except OSError:
+            pass
+        plot(
+            n_time=sim.params['procs'][1],
+            n_space=sim.params['procs'][2],
+            useGPU=sim.params['useGPU'],
+            base_path=args.base_path,
+            space_range=range(sim.params['procs'][2]),
+            n_frames=args.nframes,
+            res=sim.params['res'],
+            start_frame=args.start,
+        )
     elif args.mode == 'video':
-        for view in [None, 'xy', 'xz', 'yz']:
+        for view in [None, 'slice']:  #'xy', 'xz', 'yz']:
             video(view)
     else:
         raise NotImplementedError()
-
-
