5fcddbe8d254f8097a4840498dd0dfc0b966e4ec
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2025-01-22 09:45:21 +0000

    Implemented @tlunet's suggestions

--- pySDC/helpers/fieldsIO.py
+++ pySDC/helpers/fieldsIO.py
@@ -69,11 +69,30 @@ except ImportError:
 DTYPES = {
     0: np.float64,  # double precision
     1: np.complex128,
-    2: np.longdouble,  # quadruple precision
-    3: np.clongdouble,
-    4: np.float32,  # single precision
-    5: np.complex64,
 }
+try:
+    DTYPES.update(
+        {
+            2: np.float128,  # quadruple precision
+            3: np.complex256,
+        }
+    )
+except AttributeError:
+    import logging
+
+    logging.getLogger('FieldsIO').debug('Warning: Quadruple precision not available on this machine')
+try:
+    DTYPES.update(
+        {
+            4: np.float32,  # single precision
+            5: np.complex64,
+        }
+    )
+except AttributeError:
+    import logging
+
+    logging.getLogger('FieldsIO').debug('Warning: Single precision not available on this machine')
+
 DTYPES_AVAIL = {val: key for key, val in DTYPES.items()}
 
 # Header dtype
@@ -100,7 +119,7 @@ class FieldsIO:
         fileName : str
             File.
         """
-        assert dtype in DTYPES_AVAIL, f"{dtype=} not available"
+        assert dtype in DTYPES_AVAIL, f"{dtype=} not available. Supported on this machine: {list(DTYPES_AVAIL.keys())}"
         self.dtype = dtype
         self.fileName = fileName
         self.initialized = False
