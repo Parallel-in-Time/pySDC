0f0a0a7f4dda6d8b8f860f71779b0e91c069c186
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2025-06-25 14:31:42 +0200

    restricting mpi4py version number

--- pySDC/projects/compression/Docker/install_pySDC.sh
+++ pySDC/projects/compression/Docker/install_pySDC.sh
@@ -9,18 +9,13 @@ source /opt/spack/share/spack/setup-env.sh
 # load libpressio in spack to make sure we are using the correct Python
 spack load libpressio
 
-which mpicc
-mpicc --version
-
 # install local version of pySDC and other dependencies
 python -m pip install --upgrade pip
 cd /pySDC
 pip install -e .
 python -m pip install pytest
 python -m pip install coverage
-python -m pip install --no-binary :all: mpi4py
-
-python -c "from mpi4py import MPI"
+python -m pip install mpi4py<4.1.0
 
 # go back to original working directory
 cd $current_dir
