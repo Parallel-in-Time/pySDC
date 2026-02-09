18165fcdac5bf9d47da0a2bef794dd9da021ab29
Ouardghi <a.ouardghi@fz-juelich.de>
2026-02-09 11:19:57 +0100

    Address PR review comments: split examples, reduce duplication, improve tests

--- pySDC/projects/StroemungsRaum/run_heat_equation_FEniCS.py
+++ pySDC/projects/StroemungsRaum/run_heat_equation_FEniCS.py
@@ -1,38 +1,37 @@
-import numpy as np
 import json
 from pathlib import Path
+import dolfin as df
 
-from pySDC.projects.StroemungsRaum.problem_classes.HeatEquation_2D_FEniCS import (
-    fenics_heat2D_mass,
-    fenics_heat2D_mass_timebc,
-    fenics_heat2D,
-)
+from pySDC.projects.StroemungsRaum.problem_classes.HeatEquation_2D_FEniCS import fenics_heat2D_mass
 from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
 from pySDC.implementations.sweeper_classes.imex_1st_order_mass import imex_1st_order_mass
-from pySDC.implementations.sweeper_classes.imex_1st_order import imex_1st_order
 from pySDC.implementations.transfer_classes.BaseTransfer_mass import base_transfer_mass
 from pySDC.implementations.transfer_classes.TransferFenicsMesh import mesh_to_mesh_fenics
 
 from pySDC.helpers.stats_helper import get_sorted
 from pySDC.implementations.hooks.log_solution import LogSolution
-import pySDC.helpers.plot_helper as plt_helper
 
-import dolfin as df
-import matplotlib.pyplot as plt
-import matplotlib.tri as mtri
-import matplotlib.cm as cm
-from mpl_toolkits.mplot3d import Axes3D
-
-
-def run_simulation(ml=None, mass=None):
-
-    t0 = 0
+#def run_simulation():
+def setup(t0=None):
+    """
+    Helper routine to set up parameters
+    
+    Args:
+        t0: float,
+            initial time
+    
+    Returns:
+        description: dict,
+            pySDC description dictionary containing problem and method parameters.
+        controller_params: dict,
+            Parameters for the pySDC controller.
+    """
+    # time step size
     dt = 0.2
-    Tend = 1.0
 
     # initialize level parameters
     level_params = dict()
-    level_params['restol'] = 1e-10
+    level_params['restol'] = 1e-12
     level_params['dt'] = dt
 
     # initialize step parameters
@@ -46,12 +45,11 @@ def run_simulation(ml=None, mass=None):
 
     problem_params = dict()
     problem_params['nu'] = 0.1
-    problem_params['t0'] = t0  # ugly, but necessary to set up ProblemClass
+    problem_params['t0'] = t0  
     problem_params['c_nvars'] = [64]
     problem_params['family'] = 'CG'
     problem_params['order'] = [2]
     problem_params['c'] = 0.0
-    problem_params['refinements'] = [1]
 
     # initialize controller parameters
     controller_params = dict()
@@ -60,13 +58,10 @@ def run_simulation(ml=None, mass=None):
 
     # Fill description dictionary for easy hierarchy creation
     description = dict()
-    if mass:
-        description['problem_class'] = fenics_heat2D_mass_timebc
-        description['sweeper_class'] = imex_1st_order_mass
-        description['base_transfer_class'] = base_transfer_mass
-    else:
-        description['problem_class'] = fenics_heat2D
-        description['sweeper_class'] = imex_1st_order  # pass sweeper (see part B)
+    
+    description['problem_class'] = fenics_heat2D_mass
+    description['sweeper_class'] = imex_1st_order_mass
+    description['base_transfer_class'] = base_transfer_mass
 
     description['problem_params'] = problem_params
     description['sweeper_params'] = sweeper_params
@@ -74,26 +69,67 @@ def run_simulation(ml=None, mass=None):
     description['step_params'] = step_params
     description['space_transfer_class'] = mesh_to_mesh_fenics
 
+    return description, controller_params
+
+def run_simulation(description, controller_params, Tend):
+    """
+    Run the time integration for the heat equation problem.
+
+     Args:
+        description: dict,
+            pySDC problem and method description.
+        controller_params: dict,
+            Parameters for the pySDC controller.
+        Tend: float,
+            Final simulation time.
+
+    Returns:
+        P: problem instance,
+           Problem instance containing the final solution and other problem-related information. 
+        stats: dict,
+           collected runtime statistics, 
+        rel_err: float,
+           relative final-time error.
+    """
+    # get initial time from description
+    t0 = description['problem_params']['t0']
+       
     # quickly generate block of steps
     controller = controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
-
+ 
     # get initial values on finest level
     P = controller.MS[0].levels[0].prob
     uinit = P.u_exact(t0)
-
+    
+    # get exact solution at final time for error calculation
+    uex = P.u_exact(Tend)
+    
     # call main function to get things done...
     uend, stats = controller.run(u0=uinit, t0=t0, Tend=Tend)
 
-    errors = get_sorted(stats, type='error', sortby='iter')
-    residuals = get_sorted(stats, type='residual', sortby='iter')
+    # compute relative error at final time
+    rel_err = abs(uex - uend) / abs(uex)
+    
+    return P, stats, rel_err
 
-    uex = P.u_exact(Tend)
+def run_postprocessing(description, problem, stats, Tend):
+    """
+    Postprocess and store simulation results for visualization and analysis.
 
-    Rel_err = abs(uex - uend) / abs(uex)
-    print('The relative error at time ', Tend, 'is ', Rel_err)
-
+    Args:
+        description: dict,
+            pySDC description containing problem parameters.
+        problem: Problem instance,
+            Problem instance containing the final solution and other problem-related information.
+        stats: dict,
+            collected runtime statistics, 
+        Tend: float,
+            Final simulation time.
+    
+    Returns: None
+    """
     # Get the data directory
-    path = "data/"
+    path = "data/heat_equation/"
 
     # If it does not exist, create the 'data' directory at the specified path, including any necessary parent directories
     Path(path).mkdir(parents=True, exist_ok=True)
@@ -109,22 +145,34 @@ def run_simulation(ml=None, mass=None):
 
     # Get the solution at every time step sorted by time
     Solutions = get_sorted(stats, type='u', sortby='time')
-
+    
+    
     for i in range(int(Tend / description['level_params']['dt'])):
         time = (i + 1) * description['level_params']['dt']
         #
         un = Solutions[i][1]
-        ux = P.u_exact(time)
+        ux = problem.u_exact(time)
         #
         xdmffile_u.write_checkpoint(un.values, "un", time, df.XDMFFile.Encoding.HDF5, True)
         xdmffile_u.write_checkpoint(ux.values, "ux", time, df.XDMFFile.Encoding.HDF5, True)
         #
     xdmffile_u.close()
 
-    return errors, residuals
+    return None
 
 
 if __name__ == "__main__":
 
-    # errors_sdc_noM, _ = run_simulation(mass=False)
-    errors_sdc_M, _ = run_simulation(mass=True)
+    t0 = 0.0
+    Tend = 1.0
+
+    # run the setup to get description and controller parameters
+    description, controller_params = setup(t0=t0)
+    
+    # run the simulation and get the problem, stats and relative error
+    problem, stats, rel_err = run_simulation(description, controller_params, Tend)
+    print('The relative error at time ', Tend, 'is ', rel_err)
+    
+    # run postprocessing to save parameters and solution for visualization
+    run_postprocessing(description, problem, stats, Tend)
+
