1d9c4e84b7a6a2d885b070d52261a7babaab9de6
Thibaut Lunet <thibaut.lunet@tuhh.de>
2026-03-12 13:10:13 +0100

    TL: script update

--- pySDC/playgrounds/dedalus/scripts/stab_rbc.py
+++ pySDC/playgrounds/dedalus/scripts/stab_rbc.py
@@ -12,20 +12,19 @@ import numpy as np
 from pySDC.playgrounds.dedalus.timestepper import SDCIMEX
 from pySDC.playgrounds.dedalus.problems.rbc import RBCProblem3D, OutputFiles
 
-tEnd = 2
+tEnd = 1
 
 nStepsMax = 40
-nStepsMin = 22
+nStepsMin = 30
 decrement = 2
 
 nStepsVals = np.arange(nStepsMax, nStepsMin-1, -abs(decrement))
 dtVals = 1/nStepsVals
 
-
-Rayleigh = 1e7
-timeScheme = "SDC"
-stabDir = f"stab_A4_M1_R8_{timeScheme}"
-initSol = "init_3D_A4_M1_R8.pySDC"
+Rayleigh = 2e6
+timeScheme = "RK443"
+stabDir = f"wt/stab_A4_M1_R4_{timeScheme}"
+initSol = "wt/init_3D_A4_M4_R4.pySDC"
 
 SDCIMEX.setParameters(
     nNodes=4, nodeType="LEGENDRE", quadType="RADAU-RIGHT",
@@ -36,29 +35,26 @@ os.makedirs(stabDir, exist_ok=True)
 
 fmtSuffix = f":0{len(str(nStepsMax))}d"
 
-plt.figure("spectrum")
+# plt.figure("spectrum")
 for nSteps in nStepsVals:
     dtRun = 1/nSteps
     runDir = f"{stabDir}/dt_N" + ("{"+fmtSuffix+"}").format(nSteps)
-    print(f"Run folder : {runDir}")
 
-    # prob = RBCProblem3D.runSimulation(
-    #     runDir, tEnd, dtRun, timeScheme=timeScheme,
-    #     dtWrite=tEnd, initField=initSol,
-    #     aspectRatio=4, meshRatio=1, resFactor=4,
-    #     Rayleigh=Rayleigh, Prandtl=0.7,
-    #     )
+    prob = RBCProblem3D.runSimulation(
+        runDir, tEnd, dtRun, timeScheme=timeScheme,
+        dtWrite=tEnd, initField=initSol,
+        aspectRatio=4, meshRatio=1, resFactor=4,
+        Rayleigh=Rayleigh, Prandtl=0.7,
+        )
 
-    # if "tComp" in prob.infos:
-    #     break
+    if "tComp" in prob.infos:
+        break
 
     output = OutputFiles(runDir)
     spectrum = output.getSpectrum(which="all", start=1)
 
     if np.any(np.isnan(spectrum["u"])):
         break
-    plt.loglog(spectrum["kappa"], spectrum["u"], '--', label="N"+("{"+fmtSuffix+"}").format(nSteps))
+#     plt.loglog(spectrum["kappa"], spectrum["u"], label="N"+("{"+fmtSuffix+"}").format(nSteps))
 
-plt.legend()
-plt.tight_layout()
-plt.savefig("spectrum.pdf")
+# plt.legend()
