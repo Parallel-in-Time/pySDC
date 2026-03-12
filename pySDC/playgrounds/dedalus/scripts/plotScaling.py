4be2fbd409adbbdd803054ae7bad2d7d68fe7fd3
Thibaut Lunet <thibaut.lunet@tuhh.de>
2026-03-12 12:35:32 +0100

    TL: update from cluster

--- pySDC/playgrounds/dedalus/scripts/plotScaling.py
+++ pySDC/playgrounds/dedalus/scripts/plotScaling.py
@@ -10,21 +10,66 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
+# General matplotlib settings
+plt.rc('font', size=12)
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['axes.titlesize'] = 18
+plt.rcParams['axes.labelsize'] = 16
+plt.rcParams['xtick.labelsize'] = 16
+plt.rcParams['ytick.labelsize'] = 16
+plt.rcParams['xtick.major.pad'] = 5
+plt.rcParams['ytick.major.pad'] = 5
+plt.rcParams['axes.labelpad'] = 6
+plt.rcParams['markers.fillstyle'] = 'none'
+plt.rcParams['lines.markersize'] = 7.0
+plt.rcParams['lines.markeredgewidth'] = 1.5
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['mathtext.rm'] = 'serif'
+plt.rcParams['figure.max_open_warning'] = 100
+
+COLOR_CYCLE = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
 folder = (
-    "_benchJureca"
+    "_benchJusuf_64tpc_new"
+    # "_benchJureca"
     # "_benchJusuf_64tpc"
     # "_benchJusuf"
 )
 assert os.path.isdir(folder), f"{folder} is not a folder"
 
-nNodes = 4
-nSweeps = 4
-
 R = 2
-if R == 2:
-    schemes = ["RK443", "SDC", "SDC-MPI2-GT"] # + ["SDC-MPI", "SDC-MPI2"]
-elif R == 1:
-    schemes = ["RK443", "SDC", "SDC-MPI2-GT"]
+# schemes = ["RK443-BCC", "RK443-CCC", "SDC44", "SDC44-PinT-SM-BCC", "SDC44-PinT-SM-CCC", "SDC44-PinT-TM-BCC", "SDC44-PinT-TM-CCC"]
+# schemes = ["SDC", "SDC-MPI2-GT"]
+schemes = {
+
+    "SDC44": {
+        "label": "SDC44", "c": "tab:green", "ls": "x-",
+    },
+    "SDC44-PinT-SM-CCC": {
+        "label": "SDC44 PinT", "c": "tab:green", "ls": "x--"
+    },
+    "SDC23": {
+        "label": "SDC23", "c": "tab:blue", "ls": "o-",
+    },
+    "SDC23-PinT-SM-CCC": {
+        "label": "SDC23 PinT", "c": "tab:blue", "ls": "o--"
+    },
+    "RK443-BCC": {
+        "label": "RK443", "c": "tab:red", "ls": ">-", "ref": True
+    },
+    "RK111": {
+        "label": "RK111", "c": "tab:purple", "ls": "s-"
+    },
+    # "SDC44-PinT-SM-BCC": {
+    #     "label": "SDC44 SM-Block", "c": COLOR_CYCLE[0], "ls": "o-"
+    # },
+    # "SDC44-PinT-TM-BCC": {
+    #     "label": "SDC44 TM-Block", "c": COLOR_CYCLE[0], "ls": "o--"
+    # },
+    # "SDC44-PinT-TM-CCC": {
+    #     "label": "SDC44 TM-Cyclic", "c": COLOR_CYCLE[1], "ls": "x--"
+    # }
+}
 
 useNSpS = False
 nSpS = {
@@ -37,9 +82,10 @@ nSpS = {
 
 results = {}
 
-for scheme in schemes:
+for scheme in schemes.keys():
 
     files = glob.glob(f"{folder}/R{R}_{scheme}_*.json")
+    assert len(files) > 0, f"could not find files for {scheme}"
 
     results[scheme] = []
 
@@ -48,9 +94,11 @@ for scheme in schemes:
         with open(file, "r") as f:
             infos = json.load(f)
 
-        nDOF = 5*infos["Nx"]*infos["Ny"]*infos["Nz"]
+        nAll = infos["Nx"]*infos["Ny"]*infos["Nz"]
+        nFactor = nAll*np.log(nAll)
+        nFactor = nAll
         nSteps = infos["nSteps"]
-        tSim = infos["tComp"]/nDOF/nSteps
+        tSim = infos["tComp"]/nSteps/nFactor
         nP = infos["MPI_SIZE"]
         if useNSpS:
             tSim *= nSpS[scheme]
@@ -58,38 +106,50 @@ for scheme in schemes:
 
     results[scheme].sort(key=lambda p: p[0])
 
-symbols = ["o", "^", "s", "p", "*"]
+symbols = ["o", "^", "s", "p", "*"]*2
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 ls = "--" if "128tpc" in folder else "-"
 
-plt.figure("scaling"+"-nSpS"*useNSpS+f"-R{R}-{folder}")
-for scheme, sym, col in zip(results.keys(), symbols, colors):
+label="scaling"+"-nSpS"*useNSpS+f"-R{R}-{folder}"
+plt.figure(label)
+for scheme, params in schemes.items():
     res = np.array(results[scheme]).T
-    plt.loglog(*res, sym+ls, label=scheme, c=col)
-    plt.loglog(res[0], np.prod(res[:, 0])/res[0], "--", c="gray")
+    plt.loglog(*res, params["ls"], label=params["label"], c=params["c"])
+    if "ref" in params:
+        plt.loglog(res[0], np.prod(res[:, 0])/res[0], ":", c="gray")
 plt.legend()
 plt.grid(True)
 plt.xlabel("$N_{p}$")
 if useNSpS:
-    plt.ylabel("$t_{wall}/N_{DoF}/T_{sim}$")
+    plt.ylabel("$t_{wall}/T_{sim}$")
 else:
-    plt.ylabel("$t_{wall}/N_{DoF}/N_{steps}$")
+    plt.ylabel("$t_{wall}/N_{steps}/N_{DoF}$")
 plt.tight_layout()
+plt.savefig(f"{label}.pdf")
 
 
-plt.figure(f"PinT-efficiency-R{R}-{folder}")
-nProcSpace, tSDC = np.array(results["SDC"]).T
-spdIdeal = (1+(nSweeps-1)*nNodes)/(1+(nSweeps-1))
-effIdeal = spdIdeal/nNodes
-for scheme, sym, col in zip(schemes[2:], symbols, colors):
+label=f"PinT-efficiency-R{R}-{folder}"
+plt.figure(label)
+
+
+for scheme, params in schemes.items():
+    if "PinT" not in scheme:
+        continue
+    sdcScheme = scheme.split("-")[0]
+    nNodes, nSweeps = map(int, sdcScheme[3:])
+    spdIdeal = (1+(nSweeps-1)*nNodes)/(1+(nSweeps-1))
+    effIdeal = spdIdeal/nNodes
+    print(sdcScheme, spdIdeal, effIdeal)
+    nProcSpace, tSDC = np.array(results[sdcScheme]).T
     _, tSDCPinT = np.array(results[scheme]).T
     speedup = tSDC[:len(tSDCPinT)]/tSDCPinT
     efficiency = speedup/nNodes
     nPS = nProcSpace[:len(tSDCPinT)]
-    plt.semilogx(nPS, efficiency, sym+ls, c=col, label=scheme)
-plt.semilogx(nPS, 0*nPS+effIdeal, "--", c="gray")
-plt.ylim(0, 1)
+    plt.semilogx(nPS, efficiency, params["ls"], c=params["c"], label=params["label"])
+    # plt.semilogx(nPS, 0*nPS+effIdeal, "--", c="gray")
+plt.ylim(0, 1.2)
 plt.legend()
 plt.grid(True)
 plt.xlabel("$N_{p,Space}$"), plt.ylabel("PinT-Efficiency")
 plt.tight_layout()
+plt.savefig(f"{label}.pdf")
--- pySDC/playgrounds/dedalus/scripts/stab_rbc.py
+++ pySDC/playgrounds/dedalus/scripts/stab_rbc.py
@@ -12,24 +12,24 @@ import numpy as np
 from pySDC.playgrounds.dedalus.timestepper import SDCIMEX
 from pySDC.playgrounds.dedalus.problems.rbc import RBCProblem3D, OutputFiles
 
-tEnd = 10
+tEnd = 2
 
-nStepsMax = 30
-nStepsMin = 12
-decrement = 1
+nStepsMax = 40
+nStepsMin = 22
+decrement = 2
 
 nStepsVals = np.arange(nStepsMax, nStepsMin-1, -abs(decrement))
 dtVals = 1/nStepsVals
 
 
-Rayleigh = 1.5e5
-timeScheme = "RK443"
-stabDir = f"stab_A4_M1_R1_{timeScheme}"
-initSol = "init_3D_A4_M1_R1.pySDC"
+Rayleigh = 1e7
+timeScheme = "SDC"
+stabDir = f"stab_A4_M1_R8_{timeScheme}"
+initSol = "init_3D_A4_M1_R8.pySDC"
 
 SDCIMEX.setParameters(
     nNodes=4, nodeType="LEGENDRE", quadType="RADAU-RIGHT",
-    nSweeps=4, initSweep="COPY", explSweep="PIC", implSweep="MIN-SR-FLEX",
+    nSweeps=4, initSweep="COPY", explSweep="PIC", implSweep="MIN-SR-S",
     )
 
 os.makedirs(stabDir, exist_ok=True)
@@ -40,22 +40,25 @@ plt.figure("spectrum")
 for nSteps in nStepsVals:
     dtRun = 1/nSteps
     runDir = f"{stabDir}/dt_N" + ("{"+fmtSuffix+"}").format(nSteps)
+    print(f"Run folder : {runDir}")
 
-    prob = RBCProblem3D.runSimulation(
-        runDir, tEnd, dtRun, timeScheme=timeScheme,
-        dtWrite=tEnd, initField=initSol,
-        aspectRatio=4, meshRatio=1, resFactor=1,
-        Rayleigh=Rayleigh, Prandtl=0.7,
-        )
+    # prob = RBCProblem3D.runSimulation(
+    #     runDir, tEnd, dtRun, timeScheme=timeScheme,
+    #     dtWrite=tEnd, initField=initSol,
+    #     aspectRatio=4, meshRatio=1, resFactor=4,
+    #     Rayleigh=Rayleigh, Prandtl=0.7,
+    #     )
 
-    if "tComp" in prob.infos:
-        break
+    # if "tComp" in prob.infos:
+    #     break
 
     output = OutputFiles(runDir)
     spectrum = output.getSpectrum(which="all", start=1)
 
     if np.any(np.isnan(spectrum["u"])):
         break
-    plt.loglog(spectrum["kappa"], spectrum["u"], ':', label="N"+("{"+fmtSuffix+"}").format(nSteps))
+    plt.loglog(spectrum["kappa"], spectrum["u"], '--', label="N"+("{"+fmtSuffix+"}").format(nSteps))
 
 plt.legend()
+plt.tight_layout()
+plt.savefig("spectrum.pdf")
