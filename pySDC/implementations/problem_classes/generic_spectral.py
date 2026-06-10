ee378d5f3edeb80b0dff4b56ebd8ca3f4ce9a91c
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-06-10 16:05:14 +0200

    Vectorized setup of left preconditioner in spectral discretizations

--- pySDC/implementations/problem_classes/generic_spectral.py
+++ pySDC/implementations/problem_classes/generic_spectral.py
@@ -232,16 +232,12 @@ class GenericSpectralLinear(Problem):
             self.logger.debug(f'Setting up left preconditioner with {N} local degrees of freedom')
 
             # reverse Kronecker product
-            if self.spectral.useGPU:
-                import scipy.sparse as sp
-            else:
-                sp = self.spectral.sparse_lib
+            nc = self.ncomponents
 
-            R = sp.lil_matrix((self.ncomponents * N,) * 2, dtype=int)
+            rows = self.xp.arange(N * nc)
+            cols = (rows % nc) * N + rows // nc
 
-            for j in range(self.ncomponents):
-                for i in range(N):
-                    R[i * self.ncomponents + j, j * N + i] = 1
+            R = self.spectral.sparse_lib.csr_matrix((self.xp.ones(N * nc), (rows, cols)), shape=(N * nc, N * nc))
 
             self.Pl = self.spectral.sparse_lib.csc_matrix(R, dtype=complex)
 
